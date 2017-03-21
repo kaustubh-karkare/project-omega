30dd34c3e0a5ed50293292a3ca71603da96242a4
Paritosh <paritosh0807@gmail.com>
2017-03-22 03:56:04 +0530

    Fifth update to problem 1.2

--- solutions/paritosh-sharma/week-1/problem-1.2.js
+++ solutions/paritosh-sharma/week-1/problem-1.2.js
@@ -1,117 +1,108 @@
+// jshint esversion: 6
 
-var argList = [];
+var groupList = [];
+var argTypeConditions = {};
+var argTypeCaster  = {};
+var argDataType = {};
 
-// add aditional argument types here e.g 'Type': function(argValue) [{ return condition; }, 'Data Type'.
-// Enter 'true' if no conditions
-
-argTypeConditions = {
-  'String': [function(argValue) { return true; }, 'String'],
-  'Boolean': [function(argValue) { return true; }, 'Boolean'],
-  'Int': [function(argValue) { return !isNaN(argValue); }, 'Number'],
-  '+Int': [function(argValue) { return (!isNaN(argValue) && argValue * 1 >= 0); }, 'Number'] // represents + integers.
-};
-
-argTypeEnum  = {
-  'String': 0,
-  'Boolean': 1,
-  'Number': 2
-};
-
-function argTypeCaster(argType, argValue) {
-  switch (argTypeEnum[argType]) {
-    case 0:
-      return argValue;
-    case 1:
-      return true;
-    case 2:
-      return Number(argValue);
-  }
+function addArgType(argTypeObj) {
+  argTypeConditions[argTypeObj.name] = argTypeObj.condition;
+  argDataType[argTypeObj.name] = argTypeObj.dataType;
+  argTypeCaster[argTypeObj.dataType] = argTypeObj.castFunction;
 }
 
-function argBuilder(argObj) {
-  if (argObj.key === null) {
-    console.log(`Error: Key of argument '${arg}' cannot be null`);
-    process.exit(1);
+
+function argGroupBuilder(argObj) {
+  var ii;
+  for(ii = 0; ii < argObj.argList.length; ii++) {
+    if (argObj.argList[ii].key === null) {
+      throw new Error(`Key of argument '${argObj.argList[ii].arg}' cannot be null`);
+    }
+    if (!(argObj.argList[ii].type in argTypeConditions)) {
+      throw new Error(`Invalid type of argument '${argObj.argList[ii].arg}'`);
+    }
   }
-  if (!(argObj.type in argTypeConditions)) {
-    console.log(`Error: Invalid type of argument '${arg}'`);
-    process.exit(1);
-  }
-  argList.push(argObj);
+  groupList.push(argObj);
 }
 
 // argument validator function. Add additional validations here.
 
-function parsedObjValidator(tempObj) {
-
-  for (i = 0; i < argList.length; i++) {
-    if (argList[i].isRequired === true && !(argList[i].key in tempObj)) {
-      return new Error(`Value of '${argList[i].arg}'  required`);
+function parsedObjValidator(parseObj) {
+  var ii, jj, count;
+  for (ii = 0; ii < groupList.length; ii++) {
+    count = 0;
+    for (jj = 0; jj < groupList[ii].argList.length;  jj++) {
+      if (groupList[ii].argList[jj].key in parseObj) {
+        ++count;
+      }
+    }
+    if (groupList[ii].isRequired === true) {
+      if (count === 0) {
+        throw new Error(`At least one argument from group of argument '${groupList[ii].argList[0].arg}' required.`);
+      }
+    }
+    if (count > 1) {
+      throw new Error(`Multiple arguments of group same as argument '${groupList[ii].argList[0].arg}' present.`);
     }
   }
-
-  if (tempObj.remote === true && tempObj.local === true) {
-    return new Error(`Remote and Local arguments cannot be true at the same time`);
-  }
-  return true;
-}
-
-function printParsedArgs(parsedObj) {
-  console.log(parsedObj);
+return true;
 }
 
 function argParser(inputArgs) {
-  var  ii, jj, argValue, arg, tempObj = {};
+  var ii, jj, kk, argValue, arg, parseObj = {};
   outerLoop:
     for (ii = 0; ii < inputArgs.length; ii++) {
       arg = inputArgs[ii];
       if (arg.indexOf('-') !== 0) {
-        for (jj = 0; jj < argList.length; jj++) {
-          if (argList[jj].position === ii) {
-            tempObj[argList[jj].key] = arg;
-            continue outerLoop;
+        for (jj = 0; jj < groupList.length; jj++) {
+          for (kk = 0; kk < groupList[jj].argList.length; kk++) {
+            if (groupList[jj].argList[kk].position === ii) {
+              parseObj[groupList[jj].argList[kk].key] = arg;
+              continue outerLoop;
+            }
           }
         }
       } else {
-          arg = arg.split('=');
-          for (jj = 0; jj < argList.length; jj++) {
-            if (argList[jj].arg === arg[0]) {
-              flag = 1;
+        arg = arg.split('=');
+        for (jj = 0; jj < groupList.length; jj++) {
+          for (kk = 0; kk < groupList[jj].argList.length; kk++) {
+            if (groupList[jj].argList[kk].arg === arg[0]) {
               argValue = arg[1];
-              if ( argTypeConditions[argList[jj].type][0](argValue)) {
-                argValue = argTypeCaster(argTypeConditions[argList[jj].type][1], argValue);
-                tempObj[argList[jj].key] = argValue;
+              if ( argTypeConditions[groupList[jj].argList[kk].type](argValue)) {
+                argValue = argTypeCaster[argDataType[groupList[jj].argList[kk].type]](argValue);
+                parseObj[groupList[jj].argList[kk].key] = argValue;
                 continue outerLoop;
               }
             }
           }
         }
-        return new Error(`Invalid argument '${inputArgs[ii]}' entered. Check Type and Key`)
+      }
+      throw new Error(`Invalid argument '${inputArgs[ii]}' entered. Check Type and Key`);
     }
-   var validationResult = parsedObjValidator(tempObj);
-   if (validationResult instanceof Error) {
-     return validationResult;
-   }
-  return tempObj;
+  var validationResult = parsedObjValidator(parseObj);
+  return parseObj;
 }
+
+// add new arg types here
+
+addArgType({ name: 'String', condition: function(argValue) { return true; }, dataType: 'String', castFunction: function(argValue) { return argValue; } });
+addArgType({ name: 'Boolean', condition: function(argValue) { return true; }, dataType: 'Boolean', castFunction: function(argValue) { return true; } });
+addArgType({ name: 'Int', condition: function(argValue) { return !isNaN(argValue); }, dataType: 'Number', castFunction: function(argValue) { return Number(argValue); } });
+addArgType({ name: '+Int', condition: function(argValue) { return (!isNaN(argValue) && argValue * 1 >= 0); }, dataType: 'Number', castFunction: function(argValue) { return Number(argValue); } });
 
 // add new arguments here in the format argBuilder({key: 'argKey', arg: argName, type: 'argType', isRequired: true/false, position: index});
 
-argBuilder({ key: 'command', arg: null, type: 'String', isRequired: false, position: 0 });
-argBuilder({ key: 'subcommand', arg: null, type: 'String', isRequired: false, position: 1 });
-argBuilder({ key: 'key', arg: '--key', type: '+Int', isRequired: true, position: null });
-argBuilder({ key: 'name', arg: '--name', type: 'String', isRequired: false, position: null });
-argBuilder({ key: 'verbrose', arg: '-v', type: 'Boolean', isRequired: false, position: null });
-argBuilder({ key: 'local', arg: '--local', type: 'Boolean', isRequired: false, position: null });
-argBuilder({ key: 'remote', arg: '--remote', type: 'Boolean', isRequired: false, position: null });
+argGroupBuilder({ groupId: 1, isRequired: false, argList: [ { key: 'command', arg: null, type: 'String', position: 0 } ] });
+argGroupBuilder({ groupId: 2, isRequired: false, argList: [ { key: 'subcommand', arg: null, type: 'String', position: 1 } ] });
+argGroupBuilder({ groupId: 3, isRequired: true, argList: [ { key: 'key', arg: '--key', type: '+Int', position: null } ] });
+argGroupBuilder({ groupId: 4, isRequired: false, argList: [ { key: 'name', arg: '--name', type: 'String', position: null } ] });
+argGroupBuilder({ groupId: 5, isRequired: false, argList: [ { key: 'verbrose', arg: '-v', type: 'Boolean', position: null } ] });
+argGroupBuilder({ groupId: 6, isRequired: false, argList: [ { key: 'local', arg: '--local', type: 'Boolean', position: null }, { key: 'remote', arg: '--remote', type: 'Boolean', position: null } ] });
 
 var inputArgs = process.argv.slice(2);
-
-var parsedObj = argParser(inputArgs);
-
-if (parsedObj instanceof Error) {
+try {
+  var parsedObj = argParser(inputArgs);
   console.log(parsedObj);
-  process.exit(1);
+} catch(error) {
+  console.log(error);
 }
-else
-  printParsedArgs(parsedObj);
