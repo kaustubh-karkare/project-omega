b5123cab8c5f013636a0696989acceaf3443271d
aksh1618 <aakarshituppal@gmail.com>
2017-03-23 01:36:34 +0530

    Add threading
    Some other modifications to better suit the problem statement

--- solutions/aakarshit-uppal/p21/client.py
+++ solutions/aakarshit-uppal/p21/client.py
@@ -1,15 +1,21 @@
-"""."""
-import socket
-import sys
-# import pickle
+#!/usr/bin/env python3
+"""Client to send numbers and receive sum using tcp socket connection."""
 
-HOST = socket.gethostname()
-PORT = 54321
+import socket  # For socket operations
+import sys  # For argv
 
-numbers = (int(sys.argv[1]), int(sys.argv[2]))
-numbers_data_str = str(numbers)
-numbers_data = bytes(numbers_data_str, 'UTF-8')
+HOST = socket.gethostbyname(socket.gethostname())  # Get current host
+PORT = 54321  # Port number of server
+DESIRED_OPERATION = sum  # Can be any function accepting tuple
 
+# Extract and process numbers from command line
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
+numbers = (num1, num2)
+numbers_str = str(num1) + ' ' + str(num2)
+numbers_data = bytes(numbers_str, 'UTF-8')
+
+# Connect to server to send numbers and receive result
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mysocket:
     mysocket.connect((HOST, PORT))
     mysocket.sendall(numbers_data)
@@ -17,4 +23,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mysocket:
     result_data_str = str(result_data, 'UTF-8')
     result = int(result_data_str)
 
-print('Sum is', result)
+# Verify result
+assert result == DESIRED_OPERATION(numbers)
--- solutions/aakarshit-uppal/p21/server.py
+++ solutions/aakarshit-uppal/p21/server.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
-"""."""
-import socket
-import ast
+"""Server to receive numbers and return sum using tcp socket connection."""
 
-HOST = ''
-PORT = 54321
+import socket  # For socket operations
+import threading  # For Thread
+import time  # For sleep
 
+HOST = ''  # Any host
+PORT = 54321  # Arbitrary port
+DESIRED_OPERATION = sum  # Can be any function accepting tuple
+
+
+def handle_client(client_socket, client_socket_address):
+    """Find and send sum of numbers received from client_socket."""
+    with client_socket:
+        print('Connected to', client_socket_address)
+        numbers_data = client_socket.recv(1024)
+        time.sleep(2)
+        numbers_str = str(numbers_data, 'UTF-8')
+        numbers = tuple(int(num) for num in numbers_str.split())
+        result = DESIRED_OPERATION(numbers)
+        result_data_str = str(result)
+        result_data = bytes(result_data_str, 'UTF-8')
+        client_socket.sendall(result_data)
+
+
+# Create server socket to accept connections
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mysocket:
     mysocket.bind((HOST, PORT))
     mysocket.listen()
     while True:
         client_socket, client_socket_address = mysocket.accept()
-        with client_socket:
-            print('Connected to' + str(client_socket_address))
-            while True:
-                numbers_data = client_socket.recv(1024)
-                if not numbers_data:
-                    break
-                numbers_data_str = str(numbers_data, 'UTF-8')
-                numbers = ast.literal_eval(numbers_data_str)
-                result = sum(numbers)
-                result_data_str = str(result)
-                result_data = bytes(result_data_str, 'UTF-8')
-                client_socket.sendall(result_data)
+        # Start a thread to handle the client
+        threading.Thread(target=handle_client,
+                         args=(client_socket, client_socket_address)
+                         ).start()
