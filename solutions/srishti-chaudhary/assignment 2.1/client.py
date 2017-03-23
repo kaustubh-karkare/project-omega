b6d2f935c542a41bf45a076b019fb6c57fe6446b
chaudharyS <srishti_chaudhary1@yahoo.in>
2017-03-23 20:21:33 +0530

    Update client.py

--- solutions/srishti-chaudhary/assignment 2.1/client.py
+++ solutions/srishti-chaudhary/assignment 2.1/client.py
@@ -1,33 +1,40 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 import socket
 import sys
+import logging
 
+port = 2004
 # Creating TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#For maintaining a log of events
+LOG_FILENAME = 'client.log'
+logging.basicConfig(filename=LOG_FILENAME,
+                    level=logging.DEBUG,
+                    )
+
 # Connecting the socket to the port where the server is listening 
 server_address = ('localhost', 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
+logging.info('connecting to %s port %s' % server_address)
+sock.connect(server_address,port)
 
-def calculate_sum():
-	first_number = int(sys.argv[1])
-	second_number = int(sys.srgv[2])
-	result = first_number + second_number
-	return result
+first_number = int(sys.argv[1])
+second_number = int(sys.argv[2])
+client_sum = first_number + second_number
+input_data = str(first_number) + ' ' + str(second_number)
 
 try:
 	# Sending data
-	sock.sendall(sys.argv[1] + ' ' + sys.argv[2])
+	sock.sendall(input_data)
 	server_sum = int(sock.recv(1024))
-	client_sum = calculate_sum()
 	if server_sum == client_sum:
-		print >>sys.stderr, 'Result Verifired'
+		logging.info('Result Veified')
 	else:
-		print >>sys.stderr, 'Incorrect Result'
+		logging.warning('Incorrect Result')
 finally:
-	print >>sys.stderr, 'Closing socket'
+	logging.info('Closing socket')
 	sock.close()
 
 		
+	
