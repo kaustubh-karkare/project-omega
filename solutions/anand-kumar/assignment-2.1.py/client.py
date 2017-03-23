cfcde6d8ad0b89c6a20cac3ea83ed08fc84845d5
Anand Kumar <iamanandkr95@gmail.com>
2017-03-23 22:26:19 +0530

    Update client.py
    Improved style guide for client.

--- solutions/anand-kumar/assignment-2.1.py/client.py
+++ solutions/anand-kumar/assignment-2.1.py/client.py
@@ -7,7 +7,7 @@ import json
 
 
 if (len(sys.argv) < 5):
-    print 'Insufficient argument'
+    print('Insufficient argument')
     sys.exit(2)
 client_socket_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = sys.argv[1]
@@ -21,10 +21,9 @@ client_socket_object.connect((host, port))
 client_socket_object.send(json.dumps(numbers_to_be_added))
 data_from_server = client_socket_object.recv(255)
 sum_of_numbers = json.loads(data_from_server)
-if ((numbers_to_be_added['first_number']
-        + numbers_to_be_added['second_number'])
-        == sum_of_numbers['sum']):
-    print 'The server gives the correct sum:', sum_of_numbers['sum']
+if (numbers_to_be_added['first_number'] +
+        numbers_to_be_added['second_number'] == sum_of_numbers['sum']):
+    print('The server gives the correct sum: ', sum_of_numbers['sum'])
 else:
-    print 'The server gives an incorrect sum'
+    print('The server gives an incorrect sum')
 client_socket_object.close()
