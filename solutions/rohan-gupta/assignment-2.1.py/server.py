e35fb881e41175f6b3a6168af1494f95acd55ab6
gupta-rohan <rohan8030@gmail.com>
2017-03-24 14:57:15 +0530

    Update server.py
    Tried implementing multi-threading and classes.
    Kindly review.

--- solutions/rohan-gupta/assignment-2.1.py/server.py
+++ solutions/rohan-gupta/assignment-2.1.py/server.py
@@ -1,33 +1,56 @@
-import socket  
-import time  
+import socket
+import thread
+import time
+import sys
 
+
+# Class to calculate sum of the 2 numbers
+class Addition(object):
+
+    def __init__(self, express):
+        self.express = express
+
+    def answer(self):
+        # Index of white space separating the 2 numbers
+        index = self.express.index(' ')
+        i = 0
+        j = index + 1
+        num1 = ''
+        num2 = ''
+        while (i < index):
+            num1 += self.express[i]
+            i = i + 1
+        while (j > index and j < len(self.express)):
+            num2 += self.express[j]
+            j = j + 1
+        add = int(num1) + int(num2)
+        return add
+
+    def multi_thread_implementation(client_socket_obj, client_addr):
+        received_string = client_socket_obj.recv(1024)
+        Addition_obj = Addition(received_string)
+        print 'The client address is', client_addr
+        time.sleep(2)
+        server_evaluated_sum = Addition_obj.answer()
+        print 'The sum calculated by the server is', server_evaluated_sum
+        client_socket_obj.send(str(server_evaluated_sum))
 
 # Create a socket object
 server_socket_obj = socket.socket()
 # Get local machine name
-server_host = socket.gethostname()  
+server_host = sys.argv[1]
 # Reserve a port for your service
-server_port = 1011 
+server_port = int(sys.argv[2])
 # Binding server address to the socket
 server_socket_obj.bind((server_host, server_port))
 print 'Server code being executed.'
 server_socket_obj.listen(5)
 while True:
-    # Stores the sum of the 2 numbers
-    add = 0
-    # Stores each individual number
-    num_as_string = ''  
-    client_obj, client_addr = server_socket_obj.accept()
-    # Data received from the client
-    received_string = client_obj.recv(1024)  
-    received_string = received_string + ' '
-    for character in received_string:
-        if character != ' ':
-            num_as_string += character
-        else:
-            add = add + int(num_as_string)
-            num_as_string = ''
-    time.sleep(2)
-    print 'The sum calculated by the server is',add
-    client_obj.send(str(add))
-    client_obj.close()
+    client_socket_obj, client_addr = server_socket_obj.accept()
+    # Implementing multi-threading
+    try:
+        thread.start_new_thread(multi_thread_implementation,
+                                (client_socket_obj, client_addr))
+    except:
+        print 'Error: Unable to start thread'
+server_socket_obj.close()
