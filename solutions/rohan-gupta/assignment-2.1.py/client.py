ef1d0960e4e55c8682a2c9c62c153e978e3e8323
gupta-rohan <rohan8030@gmail.com>
2017-03-24 14:56:10 +0530

    Update client.py

--- solutions/rohan-gupta/assignment-2.1.py/client.py
+++ solutions/rohan-gupta/assignment-2.1.py/client.py
@@ -1,30 +1,27 @@
 import socket
+import sys
 
 
 # Create a socket object
 client_socket_obj = socket.socket()
 # Get local machine name
-client_host = socket.gethostname()  
+client_host = sys.argv[1]
 # Reserve a port for your service
-client_port = 1011
-print 'Enter first number.'  
-num1 = int(raw_input())
-print 'Enter second number.'  
-num2 = int(raw_input())
-add = str(num1 + num2);  
-print 'The sum calculated by the client is',add
+client_port = int(sys.argv[2])
+num1 = int(sys.argv[3])
+num2 = int(sys.argv[4])
+add = str(num1 + num2)
+print 'The sum calculated by the client is', add
 # The client object is connecting to the given address
 client_socket_obj.connect((client_host, client_port))
 print 'Connection to the server is made.'
 # The expression to be sent to the server is evaluated
 expression = str(num1) + ' ' + str(num2)
-client_socket_obj.send(expression)   
-evaluated_expression = client_socket_obj.recv(1024)
+client_socket_obj.send(expression)
+valuated_expression = client_socket_obj.recv(1024)
 if evaluated_expression == add:
     print 'Sum calculated by server and client is the same.'
     print 'VERIFIED.'
 else:
     print 'Sum calculated by server and client is not the same.'
     print 'NOT VERIFIED.'
-client_socket_obj.close()
-
