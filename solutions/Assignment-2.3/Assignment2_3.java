61ab386f2b7104df8282abbe164919495dc17e52
Divyansh Jamuaar <divyanshjamuaar@gmail.com>
2017-04-01 05:20:03 +0530

    Add Assignment2_3.java
    This is a class which uses HttpServer API to host a Web Server.

---
+++ solutions/Assignment-2.3/Assignment2_3.java
@@ -0,0 +1,24 @@
+/**
+ * JAVA code to implement a webserver that can handle basic GET requests.
+ * The HTTPServer waits for requests in the main thread and handles the requests
+ * in a separate ClientHandler thread.
+ */
+
+import httpserver.HttpServer;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.logging.*;
+
+class Assignment2_3 {
+
+  public static void main(String args[]) throws IOException {
+    HttpServer httpServer = new HttpServer(args[0]);
+    httpServer.start();
+    try {
+      Thread.sleep(60);
+    } catch (InterruptedException e) {}
+    httpServer.stop();
+  }
+
+}
