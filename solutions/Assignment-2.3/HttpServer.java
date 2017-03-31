c231a79e0fa4d9bd10e9b88e982483ecf965127b
Divyansh Jamuaar <divyanshjamuaar@gmail.com>
2017-04-01 05:18:23 +0530

    Update HttpServer.java
    Tried to implement the changes as requested. Now this is just an API class which will be used by Assignment2_3.java to run the server on a given port.

--- solutions/Assignment-2.3/HttpServer.java
+++ solutions/Assignment-2.3/HttpServer.java
@@ -1,60 +1,68 @@
-/**
- * JAVA code to implement a webserver that can handle basic GET requests.
- * The HTTPServer waits for requests in the main thread and handles the requests
- * in a separate ClientHandler thread.
- */
+package httpserver;
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.logging.*;
 
 // A Webserver waits for clients to connect, then starts a separate thread to handle the request.
-public class HttpServer {
+public class HttpServer extends Thread {
   private final static Logger logger = Logger.getLogger(HttpServer.class.getName());
   private static FileHandler fileHandler = null;
-  private static ServerSocket serverSocket;
-  private static int port;
+  static ServerSocket serverSocket;
+  static String inputPort;
+  static int port;
+
+  public HttpServer(String inputPort) {
+    HttpServer.inputPort = inputPort;
+  }
+
   // Method to create log file
-  public static void initLog(){
-    try {
-      fileHandler = new FileHandler("Status.log", false);
-    }
-    catch (SecurityException | IOException e) {
-      e.printStackTrace();
-    }
+  public static void initLog() throws SecurityException, IOException {
+    fileHandler = new FileHandler("Status.log", false);
     Logger logger = Logger.getLogger("");
     fileHandler.setFormatter(new SimpleFormatter());
     logger.addHandler(fileHandler);
     logger.setLevel(Level.FINE);
   }
-
-  public static void main(String args[]) throws IOException {
-    initLog();
+  // The run() method of HttpServer
+  @Override
+  public void run() {
     try {
-      port = Integer.parseInt(args[0]);
-      serverSocket = new ServerSocket(port); // Start,listen on given port
-      while (true) {
-        try {
-          logger.info("TCPServer Waiting for client on port " + port);
-          Socket socket = serverSocket.accept(); // Wait for a client to connect
-          new ClientHandler(socket); // A separate thread to handle each client
-        } catch (IOException e) {
+      initLog();
+      try {
+        port = Integer.parseInt(inputPort);
+        if (port <= 0)
+          throw new NegativeNumberException();
+        serverSocket = new ServerSocket(port); // Start,listen on given port
+        while (true) {
+          try {
+            logger.info("TCPServer Waiting for client on port " + port);
+            Socket socket = serverSocket.accept(); // Wait for a client to connect
+            new ClientHandler(socket); // A separate thread to handle each client
+          } catch (IOException e) {
             e.printStackTrace();
+          }
         }
-      }
-    } catch (NumberFormatException e) {
+      } catch (NumberFormatException | NegativeNumberException e) {
         logger.warning("Port number should be a positive integer");
-    } catch (ArrayIndexOutOfBoundsException e) {
+      } catch (ArrayIndexOutOfBoundsException e) {
         logger.warning("Port number must be entered as command line argument");
+      } catch (IOException e) {
+        System.out.println("Unable to start server on port " + port);
+      }
+    } catch (SecurityException | IOException e) {
+      System.out.println("Unable to create log file. Can't start server");
     }
-
   }
 }
 
 // A ClientHandler reads an HTTP request and responds
 class ClientHandler extends Thread {
   private static final Logger logger = Logger.getLogger(ClientHandler.class.getName());
-  static final String HTML_START = "<html>" + "<title>HTTP Server in java</title>" +
+  static final String HTML_START =
+    "<html>" +
+    "<title>HTTP Server in java</title>" +
     "<body>";
   static final String HTML_END = "</body>" + "</html>";
   private Socket connectedClient = null;
@@ -71,79 +79,100 @@ class ClientHandler extends Thread {
   public void run() {
 
     try {
-      logger.info("The Client " + connectedClient.getInetAddress() +
-        ":" + connectedClient.getPort() + " is connected");
-      inFromClient = new BufferedReader(new InputStreamReader (connectedClient.getInputStream()));
+      logger.info(
+        "The Client " +
+        connectedClient.getInetAddress() +
+        ":" +
+        connectedClient.getPort() +
+        " is connected");
+      inFromClient = new BufferedReader(new InputStreamReader(connectedClient.getInputStream()));
       outToClient = new DataOutputStream(connectedClient.getOutputStream());
-      String requestString = inFromClient.readLine();
-      String headerLine = requestString;
-      StringTokenizer tokenizer = new StringTokenizer(headerLine);
-      String httpMethod = tokenizer.nextToken();
-      String httpQueryString = tokenizer.nextToken();
-      StringBuffer responseBuffer = new StringBuffer();
-      logger.info("The HTTP request string is...<BR>");
-      responseBuffer.append("<b> This is the HTTP Server Home Page.... </b><BR>");
-      responseBuffer.append("Created by DJ<BR>");
-      while (inFromClient.ready()) {
-        // Read the HTTP Complete Query
-        logger.info(requestString);
-        requestString = inFromClient.readLine();
-      }
+      String headerLine = inFromClient.readLine();
+      String httpMethod = headerLine.substring(0,3);
+      String httpQueryString = headerLine.substring(
+        4, headerLine.indexOf(" ", headerLine.indexOf(" ") + 1));
+
       if (httpMethod.equals("GET")) {
-        if (httpQueryString.equals("/")) {
-          // The default home Page
-          sendResponse(200, responseBuffer.toString(), false);
-        }
-        else {
-          String fileName = httpQueryString.replaceFirst("/", "");
-
-          // if request is for a directory
-          if (fileName.endsWith("/")) {
-            String indexFile = fileName + "index.html";
-
-            // if index.html exists
-            if (new File(indexFile).isFile()) {
-              sendResponse(200, indexFile, true);
-            }
-            // if index.html does not exists, we send the list of files and folders in that directory
-            else {
-              try {
-                File folder = new File(fileName.substring(0, fileName.lastIndexOf('/')));
-                File[] listOfFiles = folder.listFiles();
-                StringBuffer response = new StringBuffer();
-                response.append("<b>List of files and folders</b> <BR>");
-                for (int i = 0; i < listOfFiles.length; i++) {
-                  if (listOfFiles[i].isFile()) {
-                    response.append("File - " + listOfFiles[i].getName() + "<BR>");
-                  }
-                  else if (listOfFiles[i].isDirectory()) {
-                    response.append("Folder -  " + listOfFiles[i].getName() + "<BR>");
-                  }
-                }
-                sendResponse(200, response.toString(), false);
-              } catch (NullPointerException e) {
-                sendResponse(404, "<b>" +
-                  "Error 404: The Requested resource not found ....</b>", false);
-              }
-            }
+        String requestPath = httpQueryString.replaceFirst("/", "");
+        // if request is for a directory
+        if (requestPath.endsWith("/") | httpQueryString.equals("/")) {
+          try {
+            sendDirectoryInformation(requestPath);
+          } catch (Exception e) {
+            sendResponse(404, "", false);
           }
+        }
 
-          // if request is for a file
+        // if request is for a file
+        else {
+          if (new File(requestPath).isFile()) {
+            sendResponse(200, requestPath, true);
+          }
           else {
-            if (new File(fileName).isFile()) {
-              sendResponse(200, fileName, true);
-            }
-            else {
-              sendResponse(404, "<b>" +
-                "Error 404: The Requested resource not found ....</b>", false);
-            }
+            sendResponse(404, "", false);
           }
         }
       }
-      sendResponse(404, "<b>" +
-        "Error 404: The Requested resource not found ....</b>", false);
+      sendResponse(404, "", false);
     } catch (IOException e) {
-        e.printStackTrace();
+      logger.info(
+        "The Client " +
+        connectedClient.getInetAddress() +
+        ":" +
+        connectedClient.getPort() +
+        " is disconnected");
+    }
+  }
+
+  public void sendDirectoryInformation(String requestPath) throws Exception  {
+    String indexFile = requestPath + "index.html";
+    // if index.html exists
+    if (new File(indexFile).isFile()) {
+      sendResponse(200, indexFile, true);
+    }
+    // if index.html does not exists, we send the list of files and folders in that directory
+    else {
+      File folder;
+      String inetAddress = HttpServer.serverSocket.getInetAddress().toString();
+      // If homepage is requested
+      if (requestPath.equals("")) {
+        requestPath = ".";
+        folder = new File(requestPath);
+      }
+      else
+        folder = new File(requestPath.substring(0, requestPath.lastIndexOf('/')));
+      File[] listOfFiles = folder.listFiles();
+      StringBuffer response = new StringBuffer();
+      response.append("<b>List of files and folders</b> <BR>");
+      response.append("<ul>");
+      for (int i = 0; i < listOfFiles.length; i++) {
+        if (listOfFiles[i].isFile()) {
+          response.append("<li>File - " +
+            "<a href=" +
+            inetAddress.substring(0, inetAddress.indexOf("0.0.0.0")) +
+            "/" +
+            requestPath +
+            "/" +
+            listOfFiles[i].getName() +
+            ">" +
+            listOfFiles[i].getName() +
+            "</a></li><BR>");
+        }
+        else if (listOfFiles[i].isDirectory()) {
+          response.append("<li>Folder - " +
+            "<a href=" +
+            inetAddress.substring(0, inetAddress.indexOf("0.0.0.0")) +
+            "/" +
+            requestPath +
+            "/" +
+            listOfFiles[i].getName() +
+            "/>" +
+            listOfFiles[i].getName() +
+            "</a></li><BR>");
+        }
+      }
+      response.append("</ul>");
+      sendResponse(200, response.toString(), false);
     }
   }
 
@@ -151,7 +180,8 @@ class ClientHandler extends Thread {
     String statusLine = null;
     String serverdetails = "Server: Java HTTPServer";
     String contentLengthLine = null;
-    String fileName = null;
+    String requestPath = null;
+    String contentType = null;
     String contentTypeLine = "Content-Type: text/html" + "\r\n";
     FileInputStream fileInputStream = null;
 
@@ -161,11 +191,12 @@ class ClientHandler extends Thread {
       statusLine = "HTTP/1.1 404 Not Found" + "\r\n";
 
     if (isFile) {
-      fileName = responseString;
-      fileInputStream = new FileInputStream(fileName);
+      requestPath = responseString;
+      contentType = responseString.substring(responseString.lastIndexOf('.'), responseString.length());
+      fileInputStream = new FileInputStream(requestPath);
       contentLengthLine = "Content-Length: " + Integer.toString(fileInputStream.available()) + "\r\n";
-      if (!fileName.endsWith(".htm") && !fileName.endsWith(".html"))
-      contentTypeLine = "Content-Type: \r\n";
+      if (!requestPath.endsWith(".htm") && !requestPath.endsWith(".html"))
+      contentTypeLine = "Content-Type: " + contentType + "\r\n";
     }
     else {
       responseString = ClientHandler.HTML_START + responseString + ClientHandler.HTML_END;
@@ -191,10 +222,17 @@ class ClientHandler extends Thread {
     byte[] buffer = new byte[1024] ;
     int bytesRead;
 
-    while ((bytesRead = fileInputStream.read(buffer)) != -1 ) {
+    while ((bytesRead = fileInputStream.read(buffer)) != -1) {
       out.write(buffer, 0, bytesRead);
     }
     fileInputStream.close();
   }
 
+}
+
+// User defined exception class
+class NegativeNumberException extends Exception {
+  public NegativeNumberException() {
+    super("Port should be a positive integer");
+  }
 }
