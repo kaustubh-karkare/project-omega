ed2c6a44eb0ac89108f2a88bf014853b48d1a88a
lalrishav <lalrishav@gmail.com>
2017-04-05 18:24:47 +0530

    Update server.js

--- solutions/lal-rishav/Assignment-2.1/server.js
+++ solutions/lal-rishav/Assignment-2.1/server.js
@@ -1,20 +1,27 @@
 let net = require('net');
-let readline = require('readline')
+let readline = require('readline');
+let Logging = require('js-logging');
+let program = require('commander');
 
-let host = '127.0.0.1';
-let port = process.argv[2];
+program
+  .version('0.0.1')
+  .option('-p, --port <n>', 'Port as argument', parseInt)
+  .option('-i, --ip <n>', 'ip as argument', String)
+  .parse(process.argv);
+
+let logger = new Logging();
+let host = program.ip;
+let port = program.port;
 
 net.createServer(function(socket){
-	console.log('Connected: '+socket.remoteAddress + ':' + socket.remotePort);
+	logger.info('Connected: '+ socket.remoteAddress + ':' + socket.remotePort);
 	socket.on('data',function(data){
 		let value = parseInt(String(data)[0]) + parseInt(String(data)[2]);
 		socket.write(String(value));
 	})
 	socket.on('close',function(data){
-	console.log('CLOSED')
+	logger.info('Connection Closed')
 	})
 }).listen(port,host);
 
-
-
-console.log('server listining on port '+port);
+logger.info('server listining on port '+port);
