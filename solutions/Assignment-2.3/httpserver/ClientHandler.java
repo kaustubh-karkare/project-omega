b170084963509f67825d485bdfe156efa482e23e
Divyansh Jamuaar <divyanshjamuaar@gmail.com>
2017-04-06 00:32:20 +0530

    Update ClientHandler.java

--- solutions/Assignment-2.3/httpserver/ClientHandler.java
+++ solutions/Assignment-2.3/httpserver/ClientHandler.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.logging.*;
-import httpserver.*;
 
 // A ClientHandler reads an HTTP request and responds
 public class ClientHandler extends Thread {
@@ -14,10 +13,19 @@ public class ClientHandler extends Thread {
     "<title>HTTP Server in java</title>" +
     "<body>";
   static final String HTML_END = "</body>" + "</html>";
+  private static HashMap<Integer, String> statusCodes;
+  // Static block to initialize the HashMap.
+  static {
+    statusCodes = new HashMap<Integer, String>();
+    statusCodes.put(200, "200 OK");
+    statusCodes.put(404, "404 Not Found");
+    statusCodes.put(500, "500 Internal Server Error");
+    statusCodes.put(415, "415 Unsupported Media Type");
+  }
+
   private Socket socket = null;
   private BufferedReader inFromClient = null;
   private DataOutputStream outToClient = null;
-  private static HashMap<Integer, String> statusCodes;
   private LinkedHashMap<String, String> headers;
 
   private enum FileSendingDecision {
@@ -30,11 +38,6 @@ public class ClientHandler extends Thread {
     this.socket = socket;
     inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     outToClient = new DataOutputStream(socket.getOutputStream());
-    statusCodes = new HashMap<Integer, String>();
-    statusCodes.put(200, "200 OK");
-    statusCodes.put(404, "404 Not Found");
-    statusCodes.put(500, "500 Internal Server Error");
-    statusCodes.put(415, "415 Unsupported Media Type");
     start();
   }
 
@@ -47,7 +50,7 @@ public class ClientHandler extends Thread {
         socket.getInetAddress() +
         ":" +
         socket.getPort() +
-        " is connected"
+        " is connected."
       );
       String headerLine = inFromClient.readLine();
       String[] tokens = headerLine.split("\\s");
@@ -60,8 +63,19 @@ public class ClientHandler extends Thread {
 
         // If request is for a directory
         if (new File(requestPath).isDirectory() | httpQueryString.isEmpty()) {
+          logger.info("Directory requested. Looking for index.html...");
+          String indexFile = requestPath + "index.html";
+
+          if (new File(indexFile).isFile()) {
+            logger.info("index.html found. Sending index.html...");
+            sendResponse(200, indexFile, FileSendingDecision.SEND_FILE);
+            return;
+          }
+          logger.info(
+            "index.html not found." +
+            " Generating list of files and folders in the requested directory."
+          );
           try {
-            logger.info("Directory requested. Looking for index.html...");
             sendDirectoryInformation(requestPath);
           } catch (Exception e) {
             logger.warning("Internal Server Error occured - " + e.getMessage());
@@ -78,13 +92,13 @@ public class ClientHandler extends Thread {
         // If request is neither for a folder or file, then it means requested resource is not found
         else {
           sendResponse(404, "", FileSendingDecision.DONT_SEND_FILE);
-          logger.info(requestPath + " - Requested resource not found");
+          logger.info(requestPath + " - Requested resource not found.");
         }
       }
 
       else {
         sendResponse(405, "", FileSendingDecision.DONT_SEND_FILE);
-        logger.info(httpMethod + "requested. Denied");
+        logger.info(httpMethod + "requested. Denied.");
       }
 
     } catch (IOException e) {
@@ -93,20 +107,12 @@ public class ClientHandler extends Thread {
         socket.getInetAddress() +
         ":" +
         socket.getPort() +
-        " is disconnected"
+        " is disconnected."
       );
     }
   }
 
   public void sendDirectoryInformation(String requestPath) throws Exception {
-    String indexFile = requestPath + "index.html";
-    // if index.html exists
-    if (new File(indexFile).isFile()) {
-      logger.info("index.html found. Sending index.html...");
-      sendResponse(200, indexFile, FileSendingDecision.SEND_FILE);
-      return;
-    }
-    logger.info("index.html not found. Generating list of files and folders in the requested directory");
     File folder;
     // If homepage is requested
     if (requestPath.equals("")) {
@@ -125,8 +131,7 @@ public class ClientHandler extends Thread {
         String fileName = listOfFiles[ii].getName();
         response.append(
           "<li>File - " +
-          "<a href=" +
-          "/" +
+          "<a href=/" +
           path +
           ">" +
           fileName +
@@ -138,8 +143,7 @@ public class ClientHandler extends Thread {
         String folderName = listOfFiles[ii].getName();
         response.append(
           "<li>Folder - " +
-          "<a href=" +
-          "/" +
+          "<a href=/" +
           path +
           "/>" +
           folderName +
@@ -158,41 +162,40 @@ public class ClientHandler extends Thread {
 
     FileInputStream fileInputStream = null;
     final String newLine = "\r\n";
-    //String requestPath = "";
     headers = new LinkedHashMap<String, String>();
 
     headers.put("Server", "Java HTTPServer");
 
     if (decision == FileSendingDecision.SEND_FILE) {
-      //requestPath = responseString;
       String contentType = responseString.substring(
         responseString.lastIndexOf('.'),
         responseString.length()
       );
 
-      headers.put("Content-Type", contentType + newLine);
+      headers.put("Content-Type", contentType);
       fileInputStream = new FileInputStream(responseString);
       headers.put(
         "Content-Length",
-        Integer.toString(fileInputStream.available()) +
-        newLine
+        Integer.toString(fileInputStream.available())
       );
 
     }
     else {
-      headers.put("Content-Type", "text/html" + newLine);
-      headers.put("Content-Length", responseString.length() + newLine);
+      headers.put("Content-Type", "text/html");
+      headers.put("Content-Length", Integer.toString(responseString.length()));
     }
 
-    headers.put("Connection", "close" + newLine + newLine);
-    outToClient.writeBytes("HTTP/1.1 " + statusCodes.get(statusCode));
+    headers.put("Connection", "close");
+    outToClient.writeBytes("HTTP/1.1 " + statusCodes.get(statusCode) + newLine);
 
-    for (Map.Entry ii: headers.entrySet()) {
-      outToClient.writeBytes(ii.getKey().toString());
+    for (Map.Entry header: headers.entrySet()) {
+      outToClient.writeBytes(header.getKey().toString());
       outToClient.writeBytes(": ");
-      outToClient.writeBytes(ii.getValue().toString());
+      outToClient.writeBytes(header.getValue().toString());
+      outToClient.writeBytes(newLine);
     }
 
+    outToClient.writeBytes(newLine);
     logger.info("Headers sent successfully");
 
     if (decision == FileSendingDecision.SEND_FILE) {
