1f0fd61e7ae334a397b47de9667d68e0de8eb8bc
Divyansh Jamuaar <divyanshjamuaar@gmail.com>
2017-04-05 12:06:10 +0530

    Update Assignment2_3.java

--- solutions/Assignment-2.3/Assignment2_3.java
+++ solutions/Assignment-2.3/Assignment2_3.java
@@ -5,8 +5,8 @@
   * A third party package - argparse4j (Command Line Parser) has been used so it must be
   * installed inorder to run the webserver.
   *
-  * Two command line arguments are needed during the execution in the recommended format as shown -
-  *   java Assignment2_3 --P PORT_NUMBER --L LOG_CONFIGURATION_FILE_PATH
+  * Four command line arguments(optional) are needed during the execution in the recommended format as shown -
+  *   java Assignment2_3 --P PORT_NUMBER --IP IP_ADDRESS --L LOG_CONFIGURATION_FILE_PATH --B BACKLOG
   *
   * PORT_NUMBER is the port on which the server will be hosted.
   *
@@ -27,35 +27,59 @@ import java.util.*;
 import java.util.logging.*;
 import net.sourceforge.argparse4j.*;
 import net.sourceforge.argparse4j.inf.*;
+
 class Assignment2_3 {
   private static final Logger logger = Logger.getLogger(Assignment2_3.class.getName());
+  private static int port;
+  private static String ipAddress;
+  private static int backlog;
+  private static String logConfigurationFilePath;
 
   public static void main(String args[]) throws IOException {
-    int port;
-    String logConfigurationFilePath;
+
     ArgumentParser parser = ArgumentParsers.newArgumentParser("Server")
       .defaultHelp(true)
       .description("Java Server");
 
-    parser.addArgument("--Port") // Any abbreviation may be used starting with same characters
-            .type(Integer.class)
-            .setDefault(8080)
-            .dest("port")
-            .help("Port number on which server will be hosted");
+    parser.addArgument("--IP") // Any abbreviation may be used starting with same characters
+      .type(String.class)
+      .dest("IP")
+      .setDefault("127.0.0.1")
+      .help("IP Address on which server will be hosted");
 
-    parser.addArgument("--LogConfigurationFilePath") // Same rule for abbreviation
-            .type(String.class)
-            .dest("configPath")
-            .help("Path containing the Configuration File");
+    parser.addArgument("--Port")
+      .type(Integer.class)
+      .setDefault(8080)
+      .dest("port")
+      .help("Port number on which server will be hosted");
+
+    parser.addArgument("--Backlog")
+      .type(Integer.class)
+      .setDefault(50)
+      .dest("backlog")
+      .help("Requested maximum length of the queue of incoming connections");
+
+    parser.addArgument("--LogConfigurationFilePath")
+      .type(String.class)
+      .dest("configPath")
+      .setDefault("")
+      .help("Path containing the Configuration File");
+
+
 
     try {
       Namespace namespace = parser.parseArgs(args);
       port = namespace.getInt("port");
+      ipAddress = namespace.getString("IP");
+      backlog = namespace.getInt("backlog");
       logConfigurationFilePath = namespace.getString("configPath");
       HttpServer httpServer = new HttpServerBuilder()
-                                          .setPort(port)
-                                          .setLogConfigurationFilePath(logConfigurationFilePath)
-                                          .getHttpServer();
+        .setPort(port)
+        .setIP(ipAddress)
+        .setBacklog(backlog)
+        .setLogConfigurationFilePath(logConfigurationFilePath)
+        .getHttpServer();
+
       httpServer.start();
       try {
         Thread.sleep(60);
