9337d771087ff3d410a1cac602e07d25aa4854a7
lalrishav <lalrishav@gmail.com>
2017-04-06 02:25:29 +0530

    Update server.js

--- solutions/lal-rishav/Assignment-2.1/server.js
+++ solutions/lal-rishav/Assignment-2.1/server.js
@@ -5,23 +5,37 @@ let program = require('commander');
 
 program
   .version('0.0.1')
-  .option('-p, --port <n>', 'Port as argument', parseInt)
-  .option('-i, --ip <n>', 'ip as argument', String)
+  .option('-p, --port <>', 'Port as argument', parseInt)
+  .option('-i, --ip <>', 'ip as argument', String)
   .parse(process.argv);
 
 let logger = new Logging();
-let host = program.ip;
-let port = program.port;
+const host = program.ip;
+const port = program.port;
 
-net.createServer(function(socket){
-	logger.info('Connected: '+ socket.remoteAddress + ':' + socket.remotePort);
+let server = net.createServer(function(socket){
+	logger.info('Connected: ' + socket.remoteAddress + ':' + socket.remotePort);
 	socket.on('data',function(data){
-		let value = parseInt(String(data)[0]) + parseInt(String(data)[2]);
-		socket.write(String(value));
+
+	    data = String(data).split(" ");
+		const sum = parseInt(data[0])+parseInt(data[1]);
+
+		setTimeout(function () {
+		    socket.write(String(sum)); 
+		}, 2000); 
+
 	})
+
 	socket.on('close',function(data){
-	logger.info('Connection Closed')
+		logger.info('Connection Closed');
 	})
 }).listen(port,host);
 
-logger.info('server listining on port ' + port);
+try{
+	server.listen(port,host);
+	logger.info('server listining on port ' + port);
+}
+catch(err){
+	logger.info("Connection error");
+}
+
