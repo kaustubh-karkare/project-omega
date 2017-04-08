ed5f15affd3d583d6da08ebdd0559d3590de48a5
Anand Kumar <iamanandkr95@gmail.com>
2017-04-09 01:41:52 +0530

    Update urlparser.py

--- solutions/urlparser.py
+++ solutions/urlparser.py
@@ -3,22 +3,22 @@ import collections
 
 
 def parse_url(url):
-    pattern = (r'(https?)?(\:\/\/)?([a-zA-Z0-9-.]+)?\:?(\d+)?(.*)')
+    pattern = r'(?:[a-z]+:/+)?([a-zA-Z0-9.-]+)?(?::*)?(\d+)?(.*)?'
     url_group = re.match(pattern, url)
-    # url_group.group(1) contains protocol
-    # url_group.group(3) contains host
-    # url_group.group(4) contains port
-    # url_group.group(5) contains path
-    UrlParser = collections.namedtuple('UrlParser', 'host port path')
-    # Default port is 80(Assuming https)
-    port = 80
-    if url_group.group(4) is not None:
-        port = int(url_group.group(4))
+    UrlParser = (
+        collections.namedtuple(
+            'UrlParser',
+            'host port path'
+        )
+    )
+    # url_group.group(1) contains host
+    # url_group.group(2) contains port
+    # url_group.group(3) contains path
     parsed_url = (
         UrlParser(
+            url_group.group(1),
+            url_group.group(2),
             url_group.group(3),
-            port,
-            url_group.group(5)
         )
     )
     return parsed_url
