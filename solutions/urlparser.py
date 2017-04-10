a07ce0e67bf74c6024bc499af0ed2445cefaee6a
Anand Kumar <iamanandkr95@gmail.com>
2017-04-11 00:40:22 +0530

    Update urlparser.py

--- solutions/urlparser.py
+++ solutions/urlparser.py
@@ -5,20 +5,12 @@ import collections
 def parse_url(url):
     pattern = r'(?:[a-z]+:/+)?([a-zA-Z0-9.-]+)?(?::*)?(\d+)?(.*)?'
     url_group = re.match(pattern, url)
-    url_tuple = (
-        collections.namedtuple(
-            'url_tuple',
-            'host port path'
-        )
-    )
-    # url_group.group(1) contains host
-    # url_group.group(2) contains port
-    # url_group.group(3) contains path
+    url_tuple = collections.namedtuple('url_tuple', ['host', 'port', 'path'])
     parsed_url = (
         url_tuple(
-            url_group.group(1),
-            url_group.group(2),
-            url_group.group(3),
+            host=url_group.group(1),
+            port=url_group.group(2),
+            path=url_group.group(3),
         )
     )
     return parsed_url
