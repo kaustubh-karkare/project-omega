3a9b9f6acd63c8e9d6aa56397d9e7b5f68adc4d2
Anand Kumar <iamanandkr95@gmail.com>
2017-04-18 18:51:30 +0530

    Update assignment-2-3.py

--- solutions/assignment-2-3.py
+++ solutions/assignment-2-3.py
@@ -1,64 +1,71 @@
 import socket
 import logging
 import argparse
-import concurrent.futures
 import threading
 import time
+from os import getcwd
 from processhttprequest import ReceiveAndProcessRequest
 
 
 class Server(threading.Thread):
 
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
+    def __init__(self, host, port, base_directory):
+        try:
+            self.base_directory = base_directory
+            self.server_socket = socket.socket()
+            self.server_socket.bind((host, port))
+            self.server_socket.settimeout(1)
+        except:
+            logging.error('Server socket could not be established')
+            raise
         threading.Thread.__init__(self)
 
     def run(self):
         self.start_serving()
 
     def start_serving(self):
-        try:
-            server_socket = socket.socket()
-            server_socket.bind((self.host, self.port))
-            server_socket.settimeout(1)
-            server_socket.listen(1)
-        except:
-            logging.error('Server socket could not be established')
-            raise
+        self.server_socket.listen(1)
         self.server_listening = True
-        client_connections = []
-        # Assuming maximum simultaneous request to be 5
-        executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
         while self.server_listening:
             try:
-                client_socket, client_address = server_socket.accept()
-                client_connections.append(
-                    executor.submit(
-                        ReceiveAndProcessRequest,
-                        client_socket,
-                        client_address,
-                    )
+                self.client_socket, self.client_address = (
+                    self.server_socket.accept()
+                )
+                ReceiveAndProcessRequest(
+                    self.client_socket,
+                    self.client_address,
+                    self.base_directory
                 )
             except socket.timeout:
                 continue
             except KeyboardInterrupt:
                 self.server_listening = False
-        concurrent.futures.wait(client_connections)
-        server_socket.close()
 
     def stop_serving(self):
         self.server_listening = False
+        self.server_socket.close()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', required=True, type=str, help='Server Host')
     parser.add_argument('--port', required=True, type=int, help='Server port')
+    parser.add_argument(
+        '--base-directory',
+        '-b',
+        required=False,
+        type=str,
+        default=getcwd(),
+        help='Base Directory'
+    )
     parsed_argument = parser.parse_args()
-    server = Server(host=parsed_argument.host, port=parsed_argument.port)
+    server = Server(
+        host=parsed_argument.host,
+        port=parsed_argument.port,
+        base_directory=parsed_argument.base_directory,
+    )
     server.start()
-    time.sleep(30)
+    time.sleep(180)
     server.stop_serving()
 
 if __name__ == '__main__':
