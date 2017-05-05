8f211b2b744492da06c3da4b3503fa0bc4a25d2a
Anand Kumar <iamanandkr95@gmail.com>
2017-05-05 19:40:00 +0530

    Update filedownloader.py

--- solutions/filedownloader.py
+++ solutions/filedownloader.py
@@ -7,60 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 BLOCK_SIZE = 1024
 EOL = '\r\n'
-
-
-def receive_data_from_host(client_socket, file_content=None):
-    current_data = client_socket.recv(BLOCK_SIZE)
-    status, _, data = current_data.partition(EOL)
-    EOH = EOL + EOL
-    header_data = ''
-    previous_block = ''
-    all_headers_received = False
-    if EOH not in data:
-        header_data = data
-        previous_block = data
-    else:
-        headers, _, content = data.partition(EOH)
-        header_data += headers
-        all_headers_received = True
-        if content is not '':
-            file_content.write(content)
-    while not all_headers_received:
-        current_data = client_socket.recv(BLOCK_SIZE)
-        if not current_data:
-            break
-        if EOH not in current_data:
-            if EOH not in (current_data + previous_block):
-                header_data += current_data
-                previous_block = current_data
-            else:
-                all_headers_received = True
-                headers, _, content = \
-                    (previous_block + current_data).partition(EOH)
-                if content is not '':
-                    file_content.write(content)
-        else:
-            all_headers_received = True
-            header, _, content = current_data.partition(EOH)
-            header_data += header
-            if content is not '':
-                file_content.write(content)
-    while True:
-        content = client_socket.recv(BLOCK_SIZE)
-        if not content:
-            break
-        file_content.write(content)
-    headers = {}
-    header_data = header_data.splitlines()
-    for line in header_data:
-        if not line:
-            break
-        key, _, value = line.partition(': ')
-        headers[key] = value
-    client_socket.close()
-    if file_content is not None:
-        file_content.flush()
-    return (status, headers)
+EOH = EOL + EOL
 
 
 class FileDownloader:
@@ -78,60 +25,88 @@ class FileDownloader:
             self.url_data.port == 8080
         ):
             self.url_data = self.url_data._replace(port=80)
-        self.initialize_download()
-        downloaded_parts_name = self.download_parts()
-        self.merge_downloaded_files(downloaded_parts_name)
+        downloaded_parts_reference = self.download_parts()
+        self.merge_downloaded_files(downloaded_parts_reference)
 
-    def initialize_download(self):
-        headers = self.request_and_receive_reponse_headers()
-        try:
-            if (int(headers['Content-Length']) == 0):
-                logging.warning('No data to download')
-        except KeyError:
-            logging.warning('Download size not available')
-            self.threads = 1
-        try:
-            if (headers['Accept-Ranges'] != 'bytes'):
-                self.threads = 1
-        except KeyError:
-            self.threads = 1
-
-    def request_and_receive_reponse_headers(self):
-        client_socket = socket.socket()
-        try:
-            client_socket.connect(
-                (
-                    self.url_data.host,
-                    self.url_data.port
-                )
-            )
-        except socket.error:
-            logging.error('Client Socket connection could not be established')
-            raise
-        client_socket.send('HEAD %s HTTP/1.1%s' % (self.url_data.path, EOL))
+    def send_request(
+        self,
+        http_method,
+        client_socket,
+        start_range=0,
+        end_range=0
+    ):
+        client_socket \
+            .send('%s %s HTTP/1.1%s' % (http_method, self.url_data.path, EOL))
         headers = {}
         headers['Host'] = self.url_data.host
         headers['Connection'] = 'close'
+        if self.threads != 1:
+            headers['Range'] = \
+                'bytes=%s-%s' % (str(start_range), str(end_range))
         # Add more headers when required
         for key, value in headers.iteritems():
             client_socket.send('%s: %s%s' % (key, value, EOL))
         client_socket.send(EOL)
-        status, headers = receive_data_from_host(client_socket)
+
+    def receive_data_from_host(self, client_socket, file_content=None):
+        current_data = client_socket.recv(BLOCK_SIZE)
+        status, _, current_data = current_data.partition(EOL)
+        previous_block = ''
+        header_data = ''
+        while current_data:
+            if EOH in current_data:
+                headers, _, current_data = current_data.partition(EOH)
+                header_data += headers
+                break
+            elif EOH in (current_data + previous_block):
+                _, _, current_data = \
+                    (current_data + previous_block).partition(EOH)
+                break
+            else:
+                header_data += current_data
+                previous_block = current_data
+            current_data = client_socket.recv(BLOCK_SIZE)
+
+        while current_data:
+            file_content.write(current_data)
+            current_data = client_socket.recv(BLOCK_SIZE)
+        headers = {}
+        header_data = header_data.splitlines()
+        for line in header_data:
+            key, _, value = line.partition(': ')
+            headers[key] = value
+        if file_content is not None:
+            file_content.flush()
+        client_socket.close()
+        return (status, headers)
+
+    def request_and_receive_reponse_headers(self):
+        client_socket = socket.socket()
+        try:
+            client_socket.connect((self.url_data.host, self.url_data.port))
+        except socket.error:
+            logging.error('Client Socket connection could not be established')
+            raise
+        self.send_request('HEAD', client_socket)
+        status, headers = self.receive_data_from_host(client_socket)
         return headers
 
     def download_parts(self):
         headers = self.request_and_receive_reponse_headers()
-        try:
-            content_length = int(headers['Content-Length'])
-        except KeyError:
+        content_length = headers.get('Content-Length')
+        if content_length is None:
             content_length = 0
+            self.threads = 1
+        else:
+            content_length = int(content_length)
+        if headers.get('Accept-Ranges') is None:
+            self.threads = 1
         each_part_size = int(content_length / self.threads)
         start_range = 0
         end_range = each_part_size
-        downloaded_parts_name = []
-        ii = 1
+        downloaded_parts_reference = []
         with ThreadPoolExecutor(max_workers=self.threads) as executor:
-            while ii <= self.threads:
+            for ii in range(self.threads):
                 if end_range >= content_length:
                     end_range = content_length - 1
                 try:
@@ -139,52 +114,37 @@ class FileDownloader:
                 except:
                     logging.error('Temporary file could not be created')
                     raise
-                downloaded_parts_name.append(download_file.name)
+                downloaded_parts_reference.append(download_file)
                 client_socket = socket.socket()
                 try:
-                    client_socket.connect(
-                        (
-                            self.url_data.host,
-                            self.url_data.port
-                        )
-                    )
+                    client_socket \
+                        .connect((self.url_data.host, self.url_data.port))
                 except socket.error:
                     logging.error('Client connection could not be made')
                     raise
-                client_socket \
-                    .send('GET %s HTTP/1.1%s' % (self.url_data.path, EOL))
-                headers = {}
-                headers['Host'] = self.url_data.host
-                headers['Connection'] = 'close'
-                # Add more headers when required
-                for key, value in headers.iteritems():
-                    client_socket.send('%s: %s%s' % (key, value, EOL))
-                if self.threads != 1:
-                    client_socket.send(
-                        'Range: bytes=%s-%s%s' % (
-                            str(start_range),
-                            str(end_range),
-                            EOL
-                        )
-                    )
-                client_socket.send(EOL)
+
+                self.send_request(
+                    'GET',
+                    client_socket,
+                    start_range,
+                    end_range
+                )
                 executor.submit(
-                        receive_data_from_host,
-                        client_socket,
-                        download_file
+                    self.receive_data_from_host,
+                    client_socket,
+                    download_file
                 )
                 start_range = end_range + 1
                 end_range = start_range + each_part_size
-                ii += 1
-        return downloaded_parts_name
+        return downloaded_parts_reference
 
-    def merge_downloaded_files(self, downloaded_parts_name):
+    def merge_downloaded_files(self, downloaded_parts_reference):
         try:
             output_file = open(self.output_path, 'w')
         except IOError:
             logging.error('Output file could not be created')
             raise
-        for downloaded_file_name in downloaded_parts_name:
-            downloaded_file = open(downloaded_file_name, 'r')
-            output_file.write(downloaded_file.read())
-            downloaded_file.close()
+        for downloaded_part_reference in downloaded_parts_reference:
+            downloaded_part_reference.seek(0)
+            output_file.write(downloaded_part_reference.read())
+            downloaded_part_reference.close()
