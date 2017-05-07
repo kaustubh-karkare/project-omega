bddac7b2bb74ac358173199171352b14d9525533
Anand Kumar <iamanandkr95@gmail.com>
2017-05-07 23:24:18 +0530

    Update urlparser.py

--- solutions/urlparser.py
+++ solutions/urlparser.py
@@ -2,23 +2,25 @@ import re
 import collections
 
 
+url_tuple = (
+    collections.namedtuple(
+        'url_tuple',
+        ['protocol', 'host', 'port', 'path']
+    )
+)
+
+
 def parse_url(url):
     pattern = r'(?:([a-z]+)?(?::/+))?([a-zA-Z0-9.-]+)?(?::*)?(\d+)?(.*)?'
     url_group = re.match(pattern, url)
-    url_tuple = (
-        collections.namedtuple(
-            'url_tuple',
-            ['protocol', 'host', 'port', 'path']
-        )
-    )
-    server_port = None
+    port = None
     if url_group.group(3) is not None:
-        server_port = int(url_group.group(3))
+        port = int(url_group.group(3))
     url_data = (
         url_tuple(
             protocol=url_group.group(1),
             host=url_group.group(2),
-            port=server_port,
+            port=port,
             path=url_group.group(4),
         )
     )
