662934a40bebd885803f0af9c83204ad624688d1
Abhijeet Singh <abhijeet.singh1997@gmail.com>
2017-05-11 22:14:54 +0530

    modification

--- solutions/assignment-2.2.py
+++ solutions/assignment-2.2.py
@@ -7,65 +7,95 @@ import tempfile
 
 class DownloadManager():
 	
-	def __init__(self, url, port , parts):
+	def __init__(self, ip, port, url, parts):
 		self.threads = []
 		self.file_parts = []
 		self.logger = logging.getLogger("Download manager")
 		logging.basicConfig(level=logging.INFO)
-		self.url = url
+		if url[ : 7] == 'http://':
+			url = url[7 : ]
+		host_index = url.find('/')
+		self.host = url[ : host_index]
+		self.path = url[host_index : ]
 		self.parts = int(parts)
 		self.logger.info("Download initied")
-		self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		host_index = self.url.find('/')
-		self.host = self.url[: host_index]
-		self.path = self.url[host_index :]
-		ip = socket.gethostbyname(self.host)
+		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		if(ip == '0.0.0.0'):
+			ip_client = socket.gethostbyname(self.host)
+		else:
+			ip_client = ip
 		port = int(port)
-		self.client.connect((ip,port))
-		header = self.get_http_header(0, 0)
+		self.client.connect((ip_client,port))
+		header = self.get_http_header('HEAD', 0, 0)
 		try:
 			self.client.send(header)
-			self.client_received = self.client.recv(1024)
+			client_received = self.client.recv(1024)
+			self.header_information = {
+				'HTTP Version': client_received[ : 8], 
+				'Status': client_received[9 : 12]
+			}
+			start = 0
+			self.end_dictionary = client_received.find('\r\n\r\n')
+			while True:
+				start = client_received.find('\r\n', start)
+				tag_start = client_received.find(': ', start)
+				tag_name = client_received[start + 2 : tag_start]
+				end = client_received.find('\r\n', tag_start)
+				start = end
+				self.header_information[tag_name] = client_received[
+					tag_start + 2 : end
+				]
+				if(start == self.end_dictionary):
+					break
+			#for i in self.header_information:
+			#	print i+" : ", self.header_information[i]
 		except socket.error:
 			self.logger.warn("Download failed")
 	
 	# download starts over here
 	def start(self):
-		self.save_file = open(os.path.basename(self.url), "w")
+		self.save_file = open(os.path.basename(self.path), "w")
 		self.logger.info(
-			"Downloading File --> {}".format(os.path.basename(self.url))
+			"Downloading File --> {}".format(os.path.basename(self.path))
 		)
-		# for finding content-length from header
-		start_content_length = self.client_received.lower()
-			.find('content-length: ')
-		end_content_length = self.client_received.find(
-			'\r\n', 
-			start_content_length + 16
-		)
-		file_size = int(
-			self.client_received[start_content_length+16 : end_content_length]
-		)
-		start_byte = 0		
-		for thread_num in range(0, self.parts + 1):	
+		# for finding content-length from header information dictionary
+		if self.header_information.has_key('Content-Length'):
+			file_size = int(self.header_information['Content-Length'])
+			start_byte = 0
+			for thread_num in range(0, self.parts):	
+				temp_file = tempfile.TemporaryFile()
+				self.file_parts.append(temp_file)
+				last_byte = (file_size / self.parts) * thread_num
+				if thread_num == self.parts - 1:
+					last_byte = file_size
+				thread = threading.Thread(
+					target=self.thread_download, 
+					args=(start_byte, last_byte, temp_file)
+				)
+				start_byte = (file_size /  self.parts) * thread_num + 1
+				thread.start()
+				self.threads.append(thread)
+		else:
 			temp_file = tempfile.TemporaryFile()
 			self.file_parts.append(temp_file)
-			last_byte = (file_size / self.parts) * thread_num
-			if thread_num == self.parts:
-				last_byte = file_size
+			# -1, -1 because it has no Content-Length and therefore all 
+			# information will be downloaded in a single tempfile & thread 
 			thread = threading.Thread(
 				target=self.thread_download, 
-				args=(start_byte, last_byte, temp_file)
+				args=(-1, -1, temp_file)
 			)
-			start_byte = (file_size/self.parts) * thread_num + 1
 			thread.start()
 			self.threads.append(thread)
-			
+		
 	# waiting for all the threads to end and end the download process
 	def wait(self):
 		for thread in self.threads:
 			thread.join()
 		for file_part in self.file_parts:
-			file_part.seek(0)
+			if file_part == self.file_parts[0]:
+				file_part.seek(self.end_dictionary + 4)
+			else:
+				file_part.seek(0)
 			data = file_part.read()
 			self.save_file.write(data)
 			file_part.close()
@@ -75,7 +105,7 @@ class DownloadManager():
 	
 	# for each thread download the parts for the thread and write it to the file
 	def thread_download(self, start_byte, last_byte, temp_file):
-		header = self.get_http_header(start_byte, last_byte)
+		header = self.get_http_header('GET', start_byte, last_byte)
 		self.client.send(header)
 		while True:
 			data = self.client.recv(1024)
@@ -84,17 +114,30 @@ class DownloadManager():
 			temp_file.write(data)
 
 	# returns the http header request
-	def get_http_header(self, start_byte, last_byte):
-		if start_byte == 0 and last_byte == 0:
-			byte_range = ""
-		else: 
-			byte_range = 'bytes='+str(start_byte)+'-'+str(last_byte)		
-		header = 'GET ' + self.path + ' HTTP/1.1\r\nHost: ' + self.host
-		header += '\r\nConnection: keep-alive\r\nAccept-Encoding: gzip, '
-		header += 'deflate\r\nAccept: */*\r\nUser-Agent: python-requests/2.13.0'
-		header += '\r\nRange: ' + byte_range + '\r\n\r\n'
-		return header
-
+	def get_http_header(self, request, start_byte, last_byte):
+		next_line = '\r\n'
+		request_header = {
+			'Connection': 'keep-alive',
+			'Accept': '*/*',
+			'Accept-Encoding': 'gzip, deflate',
+			'User-Agent': 'python-requests/2.13.0'
+		}
+		header = request + ' ' + self.path + ' HTTP/1.1' + next_line
+		header += 'Host: ' + self.host + next_line + 'Connection: '
+		header += request_header['Connection'] + next_line
+		header += 'Accept-Encoding: ' + request_header['Accept-Encoding']
+		header += next_line + 'Accept: ' + request_header['Accept']
+		header += next_line + 'User-Agent: ' + request_header['User-Agent']
+		if start_byte == -1 and last_byte == -1:
+			return_header = header + next_line + next_line
+		else:
+			if start_byte == 0 and last_byte == 0:
+				byte_range = ""
+			else:
+				byte_range = 'bytes='+str(start_byte)+'-'+str(last_byte)
+			header += next_line + 'Range: ' + byte_range
+			return_header = header + next_line + next_line
+		return return_header
 	
 # takes input from user about what to download
 if __name__ == '__main__':
@@ -106,22 +149,29 @@ if __name__ == '__main__':
 		required=True
 	)
 	download_info.add_argument(
+		"-i",
+		"--ip", 
+		help ="IP on which file will be downloaded", 
+		default='0.0.0.0'
+	)
+	download_info.add_argument(
 		"-p",
 		"--port", 
 		help ="Port on which file will be downloaded", 
 		default='80'
 	)
 	download_info.add_argument(
-		"-pa", 
-		"--parts", 
-		help="Parts in which file will be downloaded", 
+		"-t", 
+		"--threads", 
+		help="Number of threads in which file will be downloaded", 
 		default='3'
 	)
-	url = download_info.parse_args().url
-	if url[:7] == 'http://':
-		url = url[7:]
-	download =	DownloadManager(url, download_info.parse_args().port,
-		download_info.parse_args().parts)
+	download = DownloadManager(
+		download_info.parse_args().ip,
+		download_info.parse_args().port, 
+		download_info.parse_args().url,
+		download_info.parse_args().threads
+	)
 	download.start()
 	download.wait()	
 
