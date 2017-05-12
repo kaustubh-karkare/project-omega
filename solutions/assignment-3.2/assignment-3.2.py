82da8c2187bd41945b0376cb82d4e92129a16abe
Anand Kumar <iamanandkr95@gmail.com>
2017-05-12 23:48:20 +0530

    Update assignment-3.2.py

--- solutions/assignment-3.2/assignment-3.2.py
+++ solutions/assignment-3.2/assignment-3.2.py
@@ -1,18 +1,34 @@
 import argparse
-from cppcompileutility import CompileCppFiles
+import os
+import time
+
+from make import Make
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        '--file_name',
+        '-f',
+        required=False,
+        type=str,
+        default='.descriptorfile',
+        help='File Name of Descriptor File'
+    )
+    parser.add_argument(
         '--directory',
         '-d',
-        required=True,
+        required=False,
         type=str,
-        help='Directory path to compile cpp files'
+        default=os.getcwd(),
+        help='Directory of Descriptor file'
     )
-    parsed_option = parser.parse_args()
-    CompileCppFiles(directory=parsed_option.directory)
+    parsed_argument = parser.parse_args()
+    descriptor_path = os.path.join(
+        parsed_argument.directory,
+        parsed_argument.file_name
+    )
+    Make(descriptor_path).start()
 
 if __name__ == '__main__':
     main()
