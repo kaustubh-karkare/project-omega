6b60dd1be98b9c1af0fac57a218f78724876799b
Anand Kumar <iamanandkr95@gmail.com>
2017-05-17 01:19:40 +0530

    Update continuous_build_system.py

--- solutions/assignment-3.2/continuous_build_system.py
+++ solutions/assignment-3.2/continuous_build_system.py
@@ -4,35 +4,40 @@ import logging
 import os
 import time
 
-from buildutility import BuildUtility
+from watcher import Watcher
 
 
 def main():
-    logging.info('Logger for continuous_build_system')
+    logging.info('Logger for continuous_build_system(cbs)')
     logger = logging.getLogger('contiuous_build_system' + str(time.ctime()))
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--configfile',
+        '--cbsconfig',
         '-f',
         required=False,
-        default=os.path.join(os.getcwd(), '.configfile'),
+        default=os.path.join(os.getcwd(), 'cbsconfig'),
         type=str,
-        help='Config file'
+        help='Continuous build system(cbs) config file'
     )
     parsed_argument = parser.parse_args()
     try:
-        with open(parsed_argument.configfile, 'r') as configfile:
+        with open(parsed_argument.cbsconfig, 'r') as cbsconfig:
             try:
-                input_data = json.load(configfile)
-                watch = input_data["watch"]
-                action = input_data["action"]
+                input_data = json.load(cbsconfig)
+                paths_to_watch = input_data.get("watch")
+                action_to_execute = input_data["action"]
             except ValueError:
-                logger.error('Key("watch"/"action") is missing')
+                logger.error('Key("action") is missing')
                 raise
     except IOError:
-        logger.error('Config file not available')
+        logger.error('Continuous build system config file not available')
         raise
-    BuildUtility(watch, action).start()
+    if paths_to_watch is None:
+        paths_to_watch = os.getcwd()
+    path_watcher = Watcher(paths_to_watch, action_to_execute)
+    path_watcher.start()
+    time.sleep(30)
+    path_watcher.stop_watcher()
 
 
 if __name__ == '__main__':
