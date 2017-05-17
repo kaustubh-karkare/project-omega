c734a7d76fc0089f0020f4d2bbad7188f295f559
Anand Kumar <iamanandkr95@gmail.com>
2017-05-17 06:34:12 +0530

    Update watchertest.py

--- solutions/assignment-3.2/test/watchertest.py
+++ solutions/assignment-3.2/test/watchertest.py
@@ -8,17 +8,17 @@ from calendar import timegm
 from watcher import Watcher
 
 DIRECTORY = tempfile.mkdtemp(dir='/tmp/')
-WATCH = DIRECTORY + "/*.txt"
-ACTION = "touch " + DIRECTORY + "/output.txt"
+WATCH = DIRECTORY + "/*.cpp"
+ACTION = "g++ -o " + DIRECTORY + "/output.o " + DIRECTORY + "/*.cpp"
 FILECOUNT = 0
 
 
-def create_txtfile():
+def create_cppfile():
     global FILECOUNT
     FILECOUNT = FILECOUNT + 1
     temp_file = tempfile.NamedTemporaryFile(dir=DIRECTORY, delete=False)
     new_file_name = \
-        os.path.join(DIRECTORY, 'testfile' + str(FILECOUNT) + '.txt')
+        os.path.join(DIRECTORY, 'testfile' + str(FILECOUNT) + '.cpp')
     old_file_name = os.path.join(DIRECTORY, temp_file.name)
     os.rename(old_file_name, new_file_name)
     return new_file_name
@@ -27,8 +27,9 @@ def create_txtfile():
 class TestWatcher(unittest.TestCase):
 
     def test_creatingfile(self):
-        path_watcher = Watcher(WATCH, ACTION)
-        output_path = os.path.join(DIRECTORY, 'output.txt')
+        path_watcher = \
+            Watcher(WATCH, ACTION)
+        output_path = os.path.join(DIRECTORY, 'output.o')
         try:
             if os.path.exists(output_path):
                 os.remove(output_path)
@@ -37,16 +38,17 @@ class TestWatcher(unittest.TestCase):
         previous_block = {}
         for element in glob.glob(WATCH):
             previous_block[element] = os.path.getmtime(element)
-        temp_file = create_txtfile()
+        temp_file = create_cppfile()
         path_watcher.start_watcher(previous_block)
         self.assertTrue(os.path.exists(output_path))
         os.remove(os.path.join(DIRECTORY, temp_file))
 
     def test_updatingfile(self):
 
-        path_watcher = Watcher(WATCH, ACTION)
-        temp_file = create_txtfile()
-        output_path = os.path.join(DIRECTORY, 'output.txt')
+        path_watcher = \
+            Watcher(WATCH, ACTION)
+        temp_file = create_cppfile()
+        output_path = os.path.join(DIRECTORY, 'output.o')
         try:
             if os.path.exists(output_path):
                 os.remove(output_path)
@@ -66,9 +68,10 @@ class TestWatcher(unittest.TestCase):
 
     def test_deletingfile(self):
 
-        path_watcher = Watcher(WATCH, ACTION)
-        temp_file = create_txtfile()
-        output_path = os.path.join(DIRECTORY, 'output.txt')
+        path_watcher = \
+            Watcher(WATCH, ACTION)
+        temp_file = create_cppfile()
+        output_path = os.path.join(DIRECTORY, 'output.o')
         try:
             if os.path.exists(output_path):
                 os.remove(output_path)
