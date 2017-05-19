d491d9d207fb2c7623d543356ae3325e19fcbfb9
Anand Kumar <iamanandkr95@gmail.com>
2017-05-20 00:20:49 +0530

    Update watcher.py

--- solutions/assignment-3.2/watcher.py
+++ solutions/assignment-3.2/watcher.py
@@ -1,5 +1,4 @@
 import os
-import logging
 import subprocess
 import time
 
@@ -10,44 +9,58 @@ from threading import Thread
 class Watcher(Thread):
 
     def __init__(
-            self,
-            paths_to_watch,
-            action_to_execute,
-            interval_between_checks=1
+        self,
+        paths_to_watch,
+        action_to_execute,
+        logger,
+        interval_between_checks=1,
     ):
-        logging.info('Logger for  Watcher')
-        self.logger = \
-            logging.getLogger(paths_to_watch + ' : ' + action_to_execute)
         self.paths_to_watch = paths_to_watch
         self.action_to_execute = action_to_execute
         self.interval_between_checks = interval_between_checks
         self.watching = False
+        self.logger = logger
         Thread.__init__(self)
 
     def run(self):
-        previous_paths = {}
+        previous_modified_time = {}
         for element in glob(self.paths_to_watch):
             try:
-                previous_paths[element] = os.path.getmtime(element)
+                previous_modified_time[element] = os.path.getmtime(element)
             except IOError:
                 self.logger.info('Path - ' + element + 'Not Available')
         self.watching = True
         while self.watching:
-            self.start_watcher(previous_paths)
+            self.run_watcher(previous_modified_time)
             time.sleep(self.interval_between_checks)
 
-    def start_watcher(self, previous_paths):
-        current_paths = {}
+    def run_watcher(self, previous_modified_time):
+        current_modified_time = {}
         for element in glob(self.paths_to_watch):
             try:
-                current_paths[element] = os.path.getmtime(element)
+                current_modified_time[element] = os.path.getmtime(element)
             except IOError:
                 self.logger.info('Path - ' + element + 'Not Available')
-        if set(previous_paths.items()) != set(current_paths.items()):
-            self.logger.info('Executing requested action')
-            subprocess.call(self.action_to_execute, shell=True)
-        previous_paths.clear()
-        previous_paths.update(current_paths)
+        action_executed = False
+        # Check if new path is added or updated.
+        for current_path, current_time in current_modified_time.items():
+            if current_path in previous_modified_time:
+                if current_time - previous_modified_time[current_path] > 0:
+                    action_executed = True
+                    subprocess.call(self.action_to_execute, shell=True)
+                    break
+                else:
+                    del previous_modified_time[current_path]
+            else:
+                action_executed = True
+                subprocess.call(self.action_to_execute, shell=True)
+                break
+        # Check if any previous path is deleted.
+        if not action_executed:
+            if previous_modified_time:
+                subprocess.call(self.action_to_execute, shell=True)
+        previous_modified_time.clear()
+        previous_modified_time.update(current_modified_time)
 
     def stop_watcher(self):
         self.watching = False
