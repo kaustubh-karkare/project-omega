ba804222b272330f1250a830b97e0667a60a2718
Anand Kumar <iamanandkr95@gmail.com>
2017-05-20 00:17:31 +0530

    Update continuous_build_system.py

--- solutions/assignment-3.2/continuous_build_system.py
+++ solutions/assignment-3.2/continuous_build_system.py
@@ -8,23 +8,30 @@ from watcher import Watcher
 
 
 def main():
-    logging.info('Logger for continuous_build_system(cbs)')
-    logger = logging.getLogger('contiuous_build_system' + str(time.ctime()))
+    logger = logging.getLogger('contiuous_build_system ' + str(time.ctime()))
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--cbsconfig',
-        '-f',
+        '-c',
         required=False,
-        default=os.path.join(os.getcwd(), 'cbsconfig'),
+        default=os.path.join(os.getcwd(), '.cbsconfig'),
         type=str,
-        help='Continuous build system(cbs) config file'
+        help='Continuous build system(cbs) config file',
+    )
+    parser.add_argument(
+        '--interval',
+        '-i',
+        required=False,
+        default=1,
+        type=float,
+        help='Interval between consecutive checks',
     )
     parsed_argument = parser.parse_args()
     try:
         with open(parsed_argument.cbsconfig, 'r') as cbsconfig:
             try:
                 input_data = json.load(cbsconfig)
-                paths_to_watch = input_data.get("watch")
+                paths_to_watch = input_data.get("watch", os.getcwd())
                 action_to_execute = input_data["action"]
             except ValueError:
                 logger.error('Key("action") is missing')
@@ -32,12 +39,20 @@ def main():
     except IOError:
         logger.error('Continuous build system config file not available')
         raise
-    if paths_to_watch is None:
-        paths_to_watch = os.getcwd()
-    path_watcher = Watcher(paths_to_watch, action_to_execute)
+    path_watcher = Watcher(
+        paths_to_watch,
+        action_to_execute,
+        logger,
+        interval_between_checks=parsed_argument.interval,
+    )
     path_watcher.start()
-    time.sleep(30)
-    path_watcher.stop_watcher()
+    logger.info('Watcher for path - ' + paths_to_watch + ' started')
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        path_watcher.stop_watcher()
+        logger.info('KeyboardInterrupt: Watcher stopped:' + paths_to_watch)
 
 
 if __name__ == '__main__':
