793afd7671aa5f1fe977932c702411b50ccfabc3
Divyansh Jamuaar <divyanshjamuaar@gmail.com>
2017-05-23 22:33:38 +0530

    Update Assignment2_2.java

--- solutions/Assignment2_2.java
+++ solutions/Assignment2_2.java
@@ -1,7 +1,7 @@
-/**	
+/**
   * JAVA program to download a file from a URL.
   * Type "java Assignment2_2 --help" on the command line for usage details.
-  * This is the driver code which uses the "downloader" package. 
+  * This is the driver code which uses the "downloader" package.
   * Here, a instance of FileDownloader class is created which receives the URL and the Thread Count.
   * Finally, the downloading process starts in a separate thread.
   */
@@ -12,45 +12,47 @@ import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 
-public class Assignment2_2 {	
-	private final static Logger logger = Logger.getLogger(Assignment2_2.class.getName());
+public class Assignment2_2 {
+	private final static Logger logger =
+		Logger.getLogger(Assignment2_2.class.getName());
 	private static String url;
 	private static int threadCount;
 	private static Namespace namespace;
 	private static FileDownloader fileDownloader;
-	
+
 	public static void main(String[] args) {
 		ArgumentParser parser = ArgumentParsers
-				.newArgumentParser("Downloader")
-				.defaultHelp(true);
-		
+			.newArgumentParser("Downloader")
+			.defaultHelp(true);
+
 		parser.addArgument("--url")
 			.type(String.class)
 			.dest("url")
 			.required(true)
 			.help("URL of the file to be downloaded");
-		
+
 		parser.addArgument("--threads")
 			.type(Integer.class)
 			.dest("threads")
 			.setDefault(4)
 			.help("Number of threads which will download the file");
-		
+
 		try {
 			namespace = parser.parseArgs(args);
 		} catch (ArgumentParserException e) {
 			logger.warning(e.getMessage());
 			return;
 		}
-		
+
 		url = namespace.getString("url");
-		threadCount = namespace.getInt("threads");		
-		fileDownloader = new FileDownloader(url, threadCount); // Initiate downloading
-		
+		threadCount = namespace.getInt("threads");
+		// Initiate downloading
+		fileDownloader = new FileDownloader(url, threadCount);
+
 		try {
 			fileDownloader.join(); // Wait for downloading to finish
 		} catch (InterruptedException e) {
 			logger.warning(e.getMessage());
-		}		
+		}
 	}
 }
