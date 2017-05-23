48c329e6526d00054e56dfbfead93f13dedc7bc7
Divyansh Jamuaar <divyanshjamuaar@gmail.com>
2017-05-23 22:34:48 +0530

    Update DownloadThread.java

--- solutions/downloader/DownloadThread.java
+++ solutions/downloader/DownloadThread.java
@@ -13,79 +13,102 @@ import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.logging.Logger;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class DownloadThread extends Thread {
-	private final static Logger logger = Logger.getLogger(FileDownloader.class.getName());	
-	private final static String newLine = "\r\n";
-	private static final int BUFFER_SIZE = 4096;
+	private final static Logger logger =
+		Logger.getLogger(FileDownloader.class.getName());
+	private final static String NEW_LINE = "\r\n";
+	protected static final int BUFFER_SIZE = 4096;
 	private int startByte;
 	private int endByte;
 	private Socket socket;
+	private URLInfo urlInfo;
 	private BufferedInputStream inFromServer = null;
 	private DataOutputStream outToServer = null;
-	private RandomAccessFile outputFile;	
-	
-	public DownloadThread(int startByte, int endByte) {		
+	private RandomAccessFile outputFile;
+
+	public DownloadThread(int startByte, int endByte, URLInfo urlInfo)
+		throws FileNotFoundException {
+		this.urlInfo = urlInfo;
 		this.startByte = startByte;
 		this.endByte = endByte;
-		
-		try {
-			outputFile = new RandomAccessFile(FileDownloader.fileName, "rw");
-		} catch (FileNotFoundException e) {
-			logger.warning(e.getMessage());
-		}
-		
+		outputFile = new RandomAccessFile(urlInfo.fileName, "rw");
+
 		this.start();
 	}
-	
-	@Override 
+
+	private void connectToServer() throws IOException {
+		socket = new Socket(urlInfo.host, urlInfo.port);
+		inFromServer = new BufferedInputStream(socket.getInputStream());
+		outToServer = new DataOutputStream(socket.getOutputStream());
+	}
+
+	@Override
 	public void run() {
 		byte data[] = new byte[BUFFER_SIZE];
 		int numRead;
-		int offset = 0;	
+		int offset = 0;
 		boolean isHeader = true;
-		
+		LinkedHashMap<String, String> headers = new LinkedHashMap<String, String>();
+		headers.put("GET", urlInfo.filePath + " HTTP/1.1");
+		headers.put("Host:", urlInfo.host);
+		headers.put("Range:", "bytes=" + startByte + "-" + endByte);
+
 		try {
-			socket = new Socket(FileDownloader.host, FileDownloader.port);
-			inFromServer = new BufferedInputStream(socket.getInputStream());
-			outToServer = new DataOutputStream(socket.getOutputStream());
-			outputFile.seek(startByte);				
-		} catch (IOException e) {			
+			connectToServer();
+			outputFile.seek(startByte);
+		} catch (IOException e) {
 			e.printStackTrace();
-		}	
-		
+		}
+
 		try {
 			// Sending "GET" request to the server for specific range
-			outToServer.writeBytes("GET " + FileDownloader.verifiedURL.getPath() + " HTTP/1.1" + newLine);		
-			outToServer.writeBytes("Host: " + FileDownloader.host + newLine);
-			outToServer.writeBytes("Range: bytes=" + startByte + "-" + endByte + newLine + newLine);
+			for (Map.Entry h : headers.entrySet()) {
+				outToServer.writeBytes(h.getKey() + " " + h.getValue() + NEW_LINE);
+  		}
+			outToServer.writeBytes(NEW_LINE);
 			outToServer.flush();
+			int total = 0;
 			
 			while ((numRead = inFromServer.read(data, 0, BUFFER_SIZE)) != -1) {
 				// Downloading the file by filtering the response headers.
 				offset = 0;
-				
+				total += numRead;
 				if (isHeader) {
 					String string = new String(data, 0, numRead);
-					int indexOfEOH = string.indexOf(newLine + newLine);
-					
-					if (indexOfEOH != -1) {							
+					int indexOfEOH = string.indexOf(NEW_LINE + NEW_LINE);
+
+					if (indexOfEOH != -1) {
 						numRead = numRead - indexOfEOH - 4;
 						offset = indexOfEOH + 4;
-						isHeader = false;						
+						isHeader = false;
 					}
-					
+
 					else {
 						numRead = 0;
 					}
 				}
-				
-				outputFile.write(data, offset, numRead);	
+
+				outputFile.write(data, offset, numRead);
 			}
-			
+
 			logger.info(this.getName() + " Executed");
 		} catch (IOException e) {
 			logger.warning(e.getMessage());
-		}		
+		}
+		finally {
+      if (outputFile != null) {
+        try {
+          outputFile.close();
+        } catch(IOException e) {}
+      }
+      if (inFromServer != null) {
+        try {
+          inFromServer.close();
+        } catch(IOException e) {}
+      }
+		}
 	}
 }
