4bc53ab722927b76d5640f46f63c1f8f82fc14df
Anand Kumar <iamanandkr95@gmail.com>
2017-05-24 04:28:56 +0530

    Update watchertest.py

--- solutions/assignment-3.2/test/watchertest.py
+++ solutions/assignment-3.2/test/watchertest.py
@@ -1,54 +1,81 @@
 import os
+import shutil
 import tempfile
+import time
 import unittest
 
 from watcher import Watcher
 
-DIRECTORY = tempfile.mkdtemp(dir='/tmp/')
-OUTPUT_PATH = tempfile.mkstemp(dir=DIRECTORY)
-PATHS_TO_WATCH = DIRECTORY
-ACTION_TO_EXECUTE = "echo True> " + OUTPUT_PATH[1]
+
+def temporary_directory():
+    original_path = os.getcwd()
+    testing_path = tempfile.mkdtemp()
+    try:
+        os.chdir(testing_path)
+        yield testing_path
+    finally:
+        os.chdir(original_path)
+        shutil.rmtree(testing_path)
 
 
 class TestWatcher(unittest.TestCase):
 
     def test_creatingpath(self):
-        path_watcher = Watcher(PATHS_TO_WATCH, ACTION_TO_EXECUTE)
-        with tempfile.TemporaryFile(dir=DIRECTORY) as _:
-            path_watcher.run_watcher()
-        with open(OUTPUT_PATH[1], 'r') as output:
-            self.assertEqual(output.read(), "True\n")
+        for testing_path in temporary_directory():
+            output_path = 'out.txt'
+            paths_to_watch = testing_path
+            action_to_execute = "echo True> " + output_path
+            path_watcher = Watcher(paths_to_watch, action_to_execute)
+            time.sleep(0.1)
+            with tempfile.TemporaryFile(dir=testing_path) as _:
+                path_watcher.run_watcher()
+            with open(output_path, 'r') as output:
+                self.assertEqual(output.read(), "True\n")
 
     def test_deletingpath(self):
-        temp_file = tempfile.mkstemp(dir=DIRECTORY)
-        path_watcher = Watcher(PATHS_TO_WATCH, ACTION_TO_EXECUTE)
-        os.remove(os.path.join(DIRECTORY, temp_file[1]))
-        path_watcher.run_watcher()
-        with open(OUTPUT_PATH[1], 'r') as output:
-            self.assertEqual(output.read(), "True\n")
+        for testing_path in temporary_directory():
+            temp_file = \
+                tempfile.NamedTemporaryFile(dir=testing_path, delete=False)
+            output_path = 'out.txt'
+            paths_to_watch = testing_path
+            action_to_execute = "echo True> " + output_path
+            path_watcher = Watcher(paths_to_watch, action_to_execute)
+            time.sleep(0.1)
+            os.remove(temp_file.name)
+            path_watcher.run_watcher()
+            with open(output_path, 'r') as output:
+                self.assertEqual(output.read(), "True\n")
 
     def test_updatingpath(self):
-        path_watcher = Watcher(PATHS_TO_WATCH, ACTION_TO_EXECUTE)
-        current_modified_time = os.path.getmtime(DIRECTORY)
-        os.utime(
-            DIRECTORY,
-            (current_modified_time + 1, current_modified_time + 1)
-        )
-        path_watcher.run_watcher()
-        with open(OUTPUT_PATH[1], 'r') as output:
-            self.assertEqual(output.read(), 'True\n')
+        for testing_path in temporary_directory():
+            output_path = 'out.txt'
+            paths_to_watch = testing_path
+            action_to_execute = "echo True> " + output_path
+            path_watcher = Watcher(paths_to_watch, action_to_execute)
+            current_modified_time = os.path.getmtime(testing_path)
+            os.utime(
+                testing_path,
+                (current_modified_time + 1, current_modified_time + 1)
+            )
+            path_watcher.run_watcher()
+            with open(output_path, 'r') as output:
+                self.assertEqual(output.read(), 'True\n')
 
     def test_updatefile_within_path(self):
-        temp_file = tempfile.mkstemp(dir=DIRECTORY)
-        path_watcher = Watcher(PATHS_TO_WATCH, ACTION_TO_EXECUTE)
-        current_modified_time = os.path.getmtime(temp_file[1])
-        os.utime(
-            temp_file[1],
-            (current_modified_time + 1, current_modified_time + 1)
-        )
-        with open(OUTPUT_PATH[1], 'w+') as output:
+        for testing_path in temporary_directory():
+            output_path = 'out.txt'
+            paths_to_watch = testing_path
+            action_to_execute = "echo True> " + output_path
+            temp_file = \
+                tempfile.NamedTemporaryFile(dir=testing_path, delete=False)
+            path_watcher = Watcher(paths_to_watch, action_to_execute)
+            current_modified_time = os.path.getmtime(temp_file.name)
+            os.utime(
+                temp_file.name,
+                (current_modified_time + 1, current_modified_time + 1),
+            )
             path_watcher.run_watcher()
-            self.assertNotEqual(output.read(), 'True\n')
+            self.assertFalse(os.path.exists(output_path))
 
 if __name__ == '__main__':
     unittest.main()
