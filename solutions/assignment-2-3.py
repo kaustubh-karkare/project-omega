4870fc994eec839702a9cf5fc7d00919e714df91
Anand Kumar <iamanandkr95@gmail.com>
2017-06-01 07:49:24 +0530

    Update assignment-2-3.py

--- solutions/assignment-2-3.py
+++ solutions/assignment-2-3.py
@@ -1,72 +1,28 @@
-import socket
-import logging
 import argparse
-import threading
+import os
 import time
-from os import getcwd
-from processhttprequest import ReceiveAndProcessRequest
 
-
-class Server(threading.Thread):
-
-    def __init__(self, host, port, base_directory):
-        try:
-            self.base_directory = base_directory
-            self.server_socket = socket.socket()
-            self.server_socket.bind((host, port))
-            self.server_socket.settimeout(1)
-        except:
-            logging.error('Server socket could not be established')
-            raise
-        threading.Thread.__init__(self)
-
-    def run(self):
-        self.start_serving()
-
-    def start_serving(self):
-        self.server_socket.listen(1)
-        self.server_listening = True
-        while self.server_listening:
-            try:
-                self.client_socket, self.client_address = (
-                    self.server_socket.accept()
-                )
-                ReceiveAndProcessRequest(
-                    self.client_socket,
-                    self.client_address,
-                    self.base_directory
-                )
-            except socket.timeout:
-                continue
-            except KeyboardInterrupt:
-                self.server_listening = False
-
-    def stop_serving(self):
-        self.server_listening = False
-        self.server_socket.close()
+from server import Server
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', required=True, type=str, help='Server Host')
+    parser.add_argument('--host', required=True, help='Server host')
     parser.add_argument('--port', required=True, type=int, help='Server port')
-    parser.add_argument(
-        '--base-directory',
-        '-b',
-        required=False,
-        type=str,
-        default=getcwd(),
-        help='Base Directory'
-    )
+    parser.add_argument('--document-root', default=os.getcwd())
     parsed_argument = parser.parse_args()
     server = Server(
-        host=parsed_argument.host,
-        port=parsed_argument.port,
-        base_directory=parsed_argument.base_directory,
+        parsed_argument.host,
+        parsed_argument.port,
+        parsed_argument.document_root,
     )
-    server.start()
-    time.sleep(180)
-    server.stop_serving()
+    server.start_serving()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        server.stop_serving()
+
 
 if __name__ == '__main__':
     main()
