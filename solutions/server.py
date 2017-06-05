d286e196a11fcaef6171543ef862e0ee5d713f69
Anand Kumar <iamanandkr95@gmail.com>
2017-06-05 10:35:37 +0530

    Update server.py

--- solutions/server.py
+++ solutions/server.py
@@ -7,28 +7,30 @@ from handleclient import HandleClient
 class Server:
 
     def __init__(self, host, port, document_root):
-        os.chdir(document_root)
         self.server_socket = socket.socket(
             socket.AF_INET,
             socket.SOCK_STREAM,
-            socket.IPPROTO_TCP
+            socket.IPPROTO_TCP,
         )
         self.server_socket.bind((host, port))
         self.server_socket.settimeout(1)
+        self.document_root = document_root
         self.server_listening = False
 
     def start_serving(self):
+        original_path = os.getcwd()
+        os.chdir(self.document_root)
         self.server_listening = True
         self.server_socket.listen(5)
         while self.server_listening:
             try:
                 client_socket, client_address = self.server_socket.accept()
-                HandleClient(client_socket, client_address,).start()
+                HandleClient(client_socket, client_address).start()
             except socket.timeout:
                 pass
             except KeyboardInterrupt:
-                self.server_listening = False
-                self.server_socket.close()
+                self.stop_serving()
+        os.chdir(original_path)
 
     def stop_serving(self):
         self.server_listening = False
