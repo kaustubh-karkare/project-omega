cac369995bdf68b3dffc55f3ba1a1a9cb0e5139f
Anand Kumar <iamanandkr95@gmail.com>
2017-06-22 22:22:27 +0530

    Update regex.py

--- solutions/src/regex.py
+++ solutions/src/regex.py
@@ -4,11 +4,10 @@ from regexparser import RegexParser
 
 def check(pattern, text):
     pattern_tokens = tokenizer(pattern)
-    root_node = RegexParser(pattern_tokens).parse_regex()
-    if root_node.match(text, 0):
+    source_node = RegexParser(pattern_tokens).parse_regex()
+    if source_node.match(text, 0):
         return True
-    else:
-        for ii in range(1, len(text)):
-            if root_node.match(text, ii):
-                return True
+    for ii in range(1, len(text)):
+        if source_node.match(text, ii):
+            return True
     return False
