f05ef71263faa2a13d1b05d638819bb6e0d86215
Anand Kumar <iamanandkr95@gmail.com>
2017-06-22 22:20:42 +0530

    Update tokenize.py

--- solutions/src/tokenize.py
+++ solutions/src/tokenize.py
@@ -1,26 +1,31 @@
+from collections import namedtuple
 from enum import Enum
 
 
 TOKENS = Enum(
     'tokens',
-    """asterisk
-    caret
-    closing_brace
-    closing_bracket
-    closing_parenthesis
-    digit
-    dollar
-    dot
-    escape_sequence
-    literal
-    minus
-    opening_brace
-    opening_bracket
-    opening_parenthesis
-    plus
-    question_mark
+    """ASTERISK
+    CARET
+    CLOSING_BRACE
+    CLOSING_BRACKET
+    CLOSING_PARENTHESIS
+    COMMA
+    DIGIT
+    DOLLAR
+    DOT
+    ESCAPE_SEQUENCE
+    LITERAL
+    MINUS
+    OPENING_BRACE
+    OPENING_BRACKET
+    OPENING_PARENTHESIS
+    PLUS
+    QUESTION_MARK
     """
 )
+
+
+Token = namedtuple('token', ['name', 'value'])
 
 
 def tokenizer(pattern):
@@ -30,37 +35,39 @@ def tokenizer(pattern):
     while index < data_length:
         token_name = None
         if pattern[index] == '*':
-            token_name = TOKENS.asterisk.name
+            token_name = TOKENS.ASTERISK.name
         elif pattern[index] == '^':
-            token_name = TOKENS.caret.name
+            token_name = TOKENS.CARET.name
         elif pattern[index] == '}':
-            token_name = TOKENS.closing_brace.name
+            token_name = TOKENS.CLOSING_BRACE.name
         elif pattern[index] == ']':
-            token_name = TOKENS.closing_bracket.name
+            token_name = TOKENS.CLOSING_BRACKET.name
         elif pattern[index] == ')':
-            token_name = TOKENS.closing_parenthesis.name
+            token_name = TOKENS.CLOSING_PARENTHESIS.name
+        elif pattern[index] == ',':
+            token_name = TOKENS.COMMA.name
         elif pattern[index] == '$':
-            token_name = TOKENS.dollar.name
+            token_name = TOKENS.DOLLAR.name
         elif pattern[index] == '.':
-            token_name = TOKENS.dot.name
+            token_name = TOKENS.DOT.name
         elif pattern[index] == '{':
-            token_name = TOKENS.opening_brace.name
+            token_name = TOKENS.OPENING_BRACE.name
         elif pattern[index] == '[':
-            token_name = TOKENS.opening_bracket.name
+            token_name = TOKENS.OPENING_BRACKET.name
         elif pattern[index] == '(':
-            token_name = TOKENS.opening_parenthesis.name
+            token_name = TOKENS.OPENING_PARENTHESIS.name
         elif pattern[index] == '+':
-            token_name = TOKENS.plus.name
+            token_name = TOKENS.PLUS.name
         elif pattern[index] == '?':
-            token_name = TOKENS.question_mark.name
+            token_name = TOKENS.QUESTION_MARK.name
         elif pattern[index] == '-':
-            token_name = TOKENS.minus.name
+            token_name = TOKENS.MINUS.name
         elif pattern[index] == '\\':
-            token_name = TOKENS.escape_sequence.name
+            token_name = TOKENS.ESCAPE_SEQUENCE.name
         elif pattern[index] >= '0' and pattern[index] <= '9':
-            token_name = TOKENS.digit.name
+            token_name = TOKENS.DIGIT.name
         else:
-            token_name = TOKENS.literal.name
-        tokens.append((token_name, pattern[index]))
+            token_name = TOKENS.LITERAL.name
+        tokens.append(Token(name=token_name, value=pattern[index]))
         index += 1
     return tokens
