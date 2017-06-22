5a30077ac1e0c892a42374bbf563043ac3bbada5
Anand Kumar <iamanandkr95@gmail.com>
2017-06-22 22:21:41 +0530

    Update regexnodes.py

--- solutions/src/regexnodes.py
+++ solutions/src/regexnodes.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-REPETITION_TYPES = Enum('repetition_types', 'greedy lazy')
+QUANTIFIER_TYPES = Enum('quantifier_types', 'GREEDY LAZY')
 
 
 class Node(object):
@@ -10,11 +10,14 @@ class Node(object):
         self.next_node = next_node
 
     def check_if_character_acceptable(self, character):
-        pass
+        raise NotImplementedError()
 
     def match(self, text, index):
-        return self.next_node.match(text, index + 1) if index < len(text) \
-            and self.check_if_character_acceptable(text[index]) else False
+        return (
+            index < len(text) and
+            self.check_if_character_acceptable(text[index]) and
+            self.next_node.match(text, index + 1)
+        )
 
 
 class Source(Node):
@@ -26,81 +29,42 @@ class Source(Node):
 class StartAnchor(Node):
 
     def match(self, text, index):
-        return self.next_node.match(text, index) if index == 0 else False
-
-
-class Literal(Node):
-
-    def __init__(self, literal):
-        self.literal = literal
-        super(Literal, self).__init__()
-
-    def check_if_character_acceptable(self, character):
-        return True if character == self.literal else False
+        return index == 0 and self.next_node.match(text, index)
 
 
 class Dot(Node):
 
     def check_if_character_acceptable(self, character):
-        return True if character != '\n' and character != '\r' else False
-
-
-class Empty(Node):
-
-    def match(self, text, index):
-        return self.next_node.match(text, index)
-
-
-class EscapeSequence(Node):
-
-    def __init__(self, escape_sequence_ranges, inverse_match=False):
-        self.escape_sequence_ranges = escape_sequence_ranges
-        self.inverse_match = inverse_match
-        super(EscapeSequence, self).__init__()
-
-    def check_if_character_acceptable(self, character):
-        character_found = False
-        for escape_sequence_range in self.escape_sequence_ranges:
-            if (
-                character >= escape_sequence_range['start'] and
-                character <= escape_sequence_range['end']
-            ):
-                character_found = True
-                break
-        if not self.inverse_match:
-            return True if character_found else False
-        return True if not character_found else False
+        return character != '\n' and character != '\r'
 
 
 class CharacterRange(Node):
 
-    def __init__(self, lower_limit, upper_limit):
-        self.lower_limit = lower_limit
-        self.upper_limit = upper_limit
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
         super(CharacterRange, self).__init__()
 
     def check_if_character_acceptable(self, character):
-        return True if character >= self.lower_limit and \
-            character <= self.upper_limit else False
+        return self.start <= character <= self.end
 
 
-class CharacterClass(Node):
+class Or(Node):
 
-    def __init__(self, character_class_items, inverse_match=False):
-        self.character_class_items = \
-            character_class_items
+    def __init__(self, character_ranges, inverse_match=False):
+        self.character_ranges = character_ranges
         self.inverse_match = inverse_match
-        super(CharacterClass, self).__init__()
+        super(Or, self).__init__()
 
     def check_if_character_acceptable(self, character):
         character_found = False
-        for acceptable_item in self.character_class_items:
-            if acceptable_item.check_if_character_acceptable(character):
+        for character_range in self.character_ranges:
+            if character_range.check_if_character_acceptable(character):
                 character_found = True
                 break
         if not self.inverse_match:
-            return True if character_found else False
-        return True if not character_found else False
+            return character_found
+        return not character_found
 
 
 class GroupStart(Node):
@@ -115,89 +79,81 @@ class GroupEnd(Node):
         return self.next_node.match(text, index)
 
 
-class AsteriskQuantifier(Node):
-
-    def __init__(
-        self,
-        repeat_path_start,
-        repeat_path_end,
-        repetition_type=REPETITION_TYPES.greedy.name,
-    ):
-        self.repeat_path_start = repeat_path_start
-        repeat_path_end.next_node = self
-        self.repetition_type = repetition_type
-        super(AsteriskQuantifier, self).__init__()
-
-    def match(self, text, index):
-        if self.repetition_type == REPETITION_TYPES.lazy.name:
-            return True if self.next_node.match(text, index) or \
-                self.repeat_path_start.match(text, index) else False
-        return True if self.repeat_path_start.match(text, index) or \
-            self.next_node.match(text, index) else False
-
-
-class QuestionMarkQuantifier(Node):
-
-    def __init__(
-        self,
-        repeat_path_start,
-        repeat_path_end,
-        repetition_type=REPETITION_TYPES.greedy.name,
-    ):
-        self.repeat_path_start = repeat_path_start
-        repeat_path_end.next_node = Empty()
-        self.repetition_type = repetition_type
-        super(QuestionMarkQuantifier, self) \
-            .__init__(repeat_path_end.next_node)
-
-    def match(self, text, index):
-        if self.repetition_type == REPETITION_TYPES.lazy.name:
-            return True if self.next_node.match(text, index) or \
-                self.repeat_path_start.match(text, index) else False
-        return True if self.repeat_path_start.match(text, index) or \
-            self.next_node.match(text, index) else False
-
-
-class BraceQuantifier(Node):
+class Repeat(Node):
 
     def __init__(
         self,
         minimum_repetition,
-        maximum_repetition,
+        maximum_repetition_limit,
         repeat_path_start,
         repeat_path_end,
+        quantifier_type=QUANTIFIER_TYPES.GREEDY.name,
     ):
         self.minimum_repetition = minimum_repetition
-        self.maximum_repetition = maximum_repetition
+        self.maximum_repetition_limit = maximum_repetition_limit
         self.number_of_repetitions_done = 0
         self.repeat_path_start = repeat_path_start
         repeat_path_end.next_node = self
-        super(BraceQuantifier, self).__init__()
+        self.quantifier_type = quantifier_type
+        super(Repeat, self).__init__()
 
     def match(self, text, index):
         if self.number_of_repetitions_done < self.minimum_repetition:
+            # If the node has been repeated less than the minimum repetition
+            # specified by the quantifer then repeat the path enclosed by the
+            # quantifier node
             self.number_of_repetitions_done += 1
             if self.repeat_path_start.match(text, index):
                 return True
             self.number_of_repetitions_done -= 1
             return False
         else:
-            if (
-                self.maximum_repetition is None or
-                self.number_of_repetitions_done < self.maximum_repetition
-            ):
-                self.number_of_repetitions_done += 1
-                if self.repeat_path_start.match(text, index):
-                    return True
-                self.number_of_repetitions_done -= 1
-            return self.next_node.match(text, index)
+            if self.quantifier_type == QUANTIFIER_TYPES.GREEDY.name:
+                return self.greedy_match(text, index)
+            else:
+                return self.lazy_match(text, index)
+
+    def greedy_match(self, text, index):
+        # The quantifier being greedy will consume as many character
+        # as possible.
+        if (
+            self.maximum_repetition_limit is None or
+            self.number_of_repetitions_done < self.maximum_repetition_limit
+        ):
+            # If their is no limit on repetition or the node has been
+            # repeated less than the maximum repetition limit then
+            # repeat the path enclosed by the quantifier.
+            self.number_of_repetitions_done += 1
+            if self.repeat_path_start.match(text, index):
+                return True
+            self.number_of_repetitions_done -= 1
+        # consume zero character and go to the next node.
+        return self.next_node.match(text, index)
+
+    def lazy_match(self, text, index):
+        # The quantifier being lazy will consume as less character as possible
+
+        # Consume zero character.
+        if self.next_node.match(text, index):
+            return True
+        if (
+            self.maximum_repetition_limit is None or
+            self.number_of_repetitions_done < self.maximum_repetition_limit
+        ):
+            # If their is no limit on repetition or the node has been
+            # repeated less than the maximum repetition limit then
+            # repeat the path enclosed by the quantifier.
+            self.number_of_repetitions_done += 1
+            if self.repeat_path_start.match(text, index):
+                return True
+            self.number_of_repetitions_done -= 1
+        return False
 
 
 class EndAnchor(Node):
 
     def match(self, text, index):
-        return self.next_node.match(text, index) \
-            if index == len(text) else False
+        return index == len(text) and self.next_node.match(text, index)
 
 
 class Destination(Node):
