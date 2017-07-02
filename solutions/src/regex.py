2e53c29f679c12fff7b0825f2f0e66d5d75079a0
Anand Kumar <iamanandkr95@gmail.com>
2017-07-02 21:38:21 +0530

    Update regex.py

--- solutions/src/regex.py
+++ solutions/src/regex.py
@@ -1,13 +1,16 @@
+from collections import namedtuple
 from tokenize import tokenizer
 from regexparser import RegexParser
 
 
+Group = namedtuple('group', ['start', 'end'])
+
+
 def check(pattern, text):
     pattern_tokens = tokenizer(pattern)
-    source_node = RegexParser(pattern_tokens).parse_regex()
-    if source_node.match(text, 0):
-        return True
-    for ii in range(1, len(text)):
-        if source_node.match(text, ii):
+    source_node, total_groups = RegexParser(pattern_tokens).parse_regex()
+    groups = [Group(start=None, end=None) for ii in range(total_groups + 1)]
+    for ii in range(0, len(text) + 1):
+        if source_node.match(text, ii, groups):
             return True
     return False
