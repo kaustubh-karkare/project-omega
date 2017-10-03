147ee0057ae7a87b173b80d1985bd291e2606708
Anand Kumar <iamanandkr95@gmail.com>
2017-10-04 03:52:48 +0530

    Update diff-utility.rb

--- solutions/anand-kumar/src/diff-utility.rb
+++ solutions/anand-kumar/src/diff-utility.rb
@@ -1,223 +1,202 @@
-require_relative 'vcs_internals'
+class Diff
 
+    def initialize(old_file_path, new_file_path, file_name=nil)
+        @old_file_path = old_file_path
+        @new_file_path = new_file_path
+        @file_name = file_name
+        @hunks_with_context = []
+        if (@file_name.nil?)
+            @file_name = @old_file_path
+        end
+        find_hunk_ranges()
+    end
 
-NodeValue = Struct.new(:key, :parent_x, :parent_y)
-
-
-def get_lines(input_file_path)
-    lines = []
-    open(input_file_path, "r") do |input_file|
+    def get_lines(file_path)
+        file_lines = []
+        open(file_path, "r") do |file|
             while true
-                data = input_file.gets
+                data = file.gets()
                 if data.nil?
                     break
                 end
-                lines.push(data)
+                file_lines.push(data.chomp())
             end
         end
-    return lines
-end
+        return file_lines
+    end
 
 
-def get_lcs(old_lines, new_lines)
-    lcs = Array.new(old_lines.length + 1) \
-        {Array.new(new_lines.length + 1, NodeValue.new(0, 0, 0))}
-    for ii in (1..old_lines.length)
-        for jj in (1..new_lines.length)
-            if old_lines[ii - 1] == new_lines[jj - 1]
-                lcs[ii][jj] = NodeValue.new(
-                    lcs[ii - 1][jj - 1].key + 1,
-                    ii - 1,
-                    jj - 1,
-                )
-            else
-                lcs[ii][jj] = NodeValue.new(lcs[ii][jj - 1].key, ii, jj - 1)
-                if lcs[ii - 1][jj].key > lcs[ii][jj].key
-                    lcs[ii][jj].key = lcs[ii - 1][jj].key
-                    lcs[ii][jj].parent_x = ii - 1
-                    lcs[ii][jj].parent_y = jj
+    def get_equivalent_lines()
+        old_lines = get_lines(@old_file_path)
+        new_lines = get_lines(@new_file_path)
+        old_line_equivalent_in_new = Array.new(old_lines.length() + 2, 0)
+        new_line_equivalent_in_old = Array.new(new_lines.length() + 2, 0)
+        old_line_equivalent_in_new[old_lines.length() + 1] = \
+            new_lines.length() + 1
+        new_line_equivalent_in_old[new_lines.length() + 1] = \
+            old_lines.length() + 1
+        lcs = Array.new(old_lines.length() + 1) \
+            {Array.new(new_lines.length() + 1, 0)}
+        preceding_position = Array.new(old_lines.length() + 1) \
+            {Array.new(new_lines.length() + 1, nil)}
+        for ii in (1..old_lines.length())
+            for jj in (1..new_lines.length())
+                if old_lines[ii - 1] == new_lines[jj - 1]
+                    lcs[ii][jj] = lcs[ii - 1][jj - 1] + 1
+                    preceding_position[ii][jj] = "up-left"
+                elsif lcs[ii - 1][jj] >= lcs[ii][jj - 1]
+                    lcs[ii][jj] = lcs[ii - 1][jj]
+                    preceding_position[ii][jj] = "up"
+                else
+                    lcs[ii][jj] = lcs[ii][jj - 1]
+                    preceding_position[ii][jj] = "left"
                 end
             end
         end
-    end
-    equivalent_line_in_new_file = Array.new(old_lines.length + 1, 0)
-    equivalent_line_in_old_file = Array.new(new_lines.length + 1, 0)
-    current_x = old_lines.length
-    current_y = new_lines.length
-    while current_x != 0 || current_y != 0
-        parent_x = lcs[current_x][current_y].parent_x
-        parent_y = lcs[current_x][current_y].parent_y
-        if current_x - 1 == parent_x && current_y - 1 == parent_y
-            equivalent_line_in_new_file[current_x] = current_y
-            equivalent_line_in_old_file[current_y] = current_x
+        ii = old_lines.length()
+        jj = new_lines.length()
+        while not preceding_position[ii][jj].nil?
+            if preceding_position[ii][jj] == "up-left"
+                old_line_equivalent_in_new[ii] = jj
+                new_line_equivalent_in_old[jj] = ii
+            end
+            if preceding_position[ii][jj] == "left"
+                jj -= 1
+            elsif preceding_position[ii][jj] == "up"
+                ii -= 1
+            else
+                ii -= 1
+                jj -= 1
+            end
         end
-        current_x = parent_x
-        current_y = parent_y
+        return old_line_equivalent_in_new, new_line_equivalent_in_old
     end
-    return equivalent_line_in_new_file, equivalent_line_in_old_file
-end
 
-
-def display_diff_delete(lines, lcs, index, line_new)
-    start_index = index
-    end_index = index
-    index += 1
-    while index <= lines.length && lcs[index] == 0 
-        end_index += 1
-        index += 1
-    end
-    if start_index == end_index
-        puts "#{start_index}d#{line_new}"
-        puts "< #{lines[start_index - 1]}"
-    else
-        puts "#{start_index},#{end_index}d#{line_new}"
-        while start_index <= end_index
-            puts "< #{lines[start_index - 1]}"
-            start_index += 1
-        end
-    end
-    return index
-end
-
-
-def display_diff_append(lines, lcs, index, line_old, line_new)
-    line_new += 1
-    start_index = index
-    end_index = index
-    index += 1
-    while index <= lines.length && lcs[index] == 0
-        index += 1
-        end_index += 1
-    end
-    if start_index == end_index    
-        puts "#{line_old}a#{line_new}"
-        puts "> #{lines[start_index - 1]}"
-    else
-        puts "#{line_old}a#{line_new},#{line_new + (end_index - start_index)}"
-        line_new  += (end_index - start_index)
-        while start_index <= end_index
-            puts "> #{lines[start_index - 1]}"
-            start_index += 1
+    def find_hunk_ranges()
+        old_line_equivalent_in_new, _new_line_equivalent_in_old = \
+            get_equivalent_lines()
+        range_start_old = 1
+        range_start_new = 1
+        old_line_number = 1
+        while old_line_number < old_line_equivalent_in_new.length()
+            if old_line_equivalent_in_new[old_line_number] != 0
+                add_context_around_hunk(
+                    range_start_old,
+                    old_line_number,
+                    range_start_new,
+                    old_line_equivalent_in_new[old_line_number],
+                )
+                range_start_old = old_line_number + 1
+                range_start_new = \
+                    old_line_equivalent_in_new[old_line_number] + 1
+            end
+            old_line_number += 1
         end
     end
-    return index, line_new
-end
 
-
-def display_diff_change(
-    old_lines,
-    new_lines,
-    equivalent_line_in_new_file,
-    equivalent_line_in_old_file,
-    start_old_index,
-    start_new_index,
-    line_new
-)
-    line_new += 1
-    start_old = start_old_index
-    start_new = start_new_index
-    end_old = start_old_index
-    end_new = start_new_index
-
-    start_old_index += 1
-    start_new_index += 1
-    while (
-        start_old_index <= old_lines.length &&
-        start_new_index <= new_lines.length &&
-        equivalent_line_in_new_file[start_old_index] == 0 &&
-        equivalent_line_in_old_file[start_new_index] == 0
+    def add_context_around_hunk(
+        range_start_old,
+        range_end_old,
+        range_start_new,
+        range_end_new,
+        lines=3
     )
-        start_old_index += 1
-        end_old += 1
-        start_new_index += 1
-        end_new += 1
-    end
-    if start_old == end_old
-        puts "#{start_old}c#{line_new}"
-        puts "< #{old_lines[start_old - 1]}"
-        puts "---"
-        puts "> #{new_lines[start_new - 1]}"
-    else
-        print "#{start_old},#{end_old}c#{line_new},"
-        print "#{line_new + (end_old - start_old)}\n"
-        line_new += (end_old - start_old)
-        while start_old <= end_old
-            puts "< #{old_lines[start_old - 1]}"
-            start_old += 1
-        end
-        puts "---"
-        while start_new <= end_new
-            puts "> #{new_lines[start_new - 1]}"
-            start_new += 1
-        end
-    end
-    return start_old_index, start_new_index, line_new
-end
-
-
-def find_diff(file1, file2)
-    old_lines = []
-    new_lines = []
-    if File.file?(file1)
-        old_lines = get_lines(file1)
-    else
-        old_lines.push(file1)
-    end
-    if File.file?(file2)
-        new_lines = get_lines(file2)
-    else
-        new_lines.push(file2)
-    end
-    equivalent_line_in_new_file, equivalent_line_in_old_file = \
-        get_lcs(old_lines, new_lines)
-    line_number_new = 0
-    old_index = 1
-    new_index = 1
-    while old_index <= old_lines.length || new_index <= new_lines.length
+        total_hunks = @hunks_with_context.length()
         if (
-            equivalent_line_in_new_file[old_index] == 0 &&
-            (
-                new_index > new_lines.length ||
-                equivalent_line_in_old_file[new_index] != 0
-            )
+            range_end_old - range_start_old == 0 and
+            range_end_new - range_start_new == 0
         )
-            old_index = display_diff_delete(
-                old_lines,
-                equivalent_line_in_new_file,
-                old_index,
-                line_number_new,
-            )
-        elsif (
-            equivalent_line_in_old_file[new_index] == 0 &&
-            (
-                old_index > old_lines.length ||
-                equivalent_line_in_new_file[old_index] != 0
-            )
-        )
-            new_index, line_number_new = display_diff_append(
-                new_lines,
-                equivalent_line_in_old_file,
-                new_index,
-                old_index - 1,
-                line_number_new,
-            )
-        elsif (
-            equivalent_line_in_new_file[old_index] == 0 &&
-            equivalent_line_in_old_file[new_index] == 0
-        )
-            old_index, new_index, line_number_new = display_diff_change(
-                old_lines,
-                new_lines,
-                equivalent_line_in_new_file,
-                equivalent_line_in_old_file,
-                old_index,
-                new_index,
-                line_number_new,
-            )
-        else equivalent_line_in_old_file[new_index] == old_index
-            line_number_new += 1
-            old_index += 1
-            new_index += 1
+            return
         end
-    end    
+        if total_hunks == 0
+            @hunks_with_context.push({
+                :range_start_old=>range_start_old,
+                :range_end_old=>range_end_old,
+                :range_start_new=>range_start_new,
+                :range_end_new=>range_end_new,
+            })
+            return
+        end
+        last_hunk = total_hunks - 1
+        if (
+            (
+                range_start_old - @hunks_with_context[last_hunk][:range_end_old]
+            ) < 2 * lines or
+            (
+                range_start_new - @hunks_with_context[last_hunk][:range_end_new]
+            ) < 2 * lines
+        )
+            @hunks_with_context[last_hunk][:range_start_old] -= lines
+            @hunks_with_context[last_hunk][:range_start_new] -= lines
+            @hunks_with_context[last_hunk][:range_start_old] = \
+                [1, @hunks_with_context[last_hunk][:range_start_old]].max
+            @hunks_with_context[last_hunk][:range_start_new] = \
+                [1, @hunks_with_context[last_hunk][:range_start_new]].max
+            @hunks_with_context[last_hunk][:range_end_old] = range_end_old
+            @hunks_with_context[last_hunk][:range_end_new] = range_end_new
+        else
+            @hunks_with_context[last_hunk][:range_end_old] += lines
+            @hunks_with_context[last_hunk][:range_end_new] += lines
+            @hunks_with_context.push({
+                :range_start_old=>range_start_old - lines,
+                :range_end_old=>range_end_old,
+                :range_start_new=>range_start_new - lines,
+                :range_end_new=>range_end_new,
+            })
+        end
+    end
+
+    def show(
+    )
+        old_line_equivalent_in_new, new_line_equivalent_in_old = \
+            get_equivalent_lines()
+        if @hunks_with_context.length() == 0
+            return
+        end
+        old_lines = get_lines(@old_file_path)
+        new_lines = get_lines(@new_file_path)
+        puts "diff a/#{@file_name} b/#{@file_name}"
+        puts "--- a/#{@file_name}"
+        puts "+++ b/#{@file_name}"
+        for ii in (0..@hunks_with_context.length() - 1)
+            range_start_old = @hunks_with_context[ii][:range_start_old]
+            range_end_old = @hunks_with_context[ii][:range_end_old]
+            range_start_new = @hunks_with_context[ii][:range_start_new]
+            range_end_new = @hunks_with_context[ii][:range_end_new]
+            print "@@ -"
+            if range_end_old - range_start_old == 0
+                print "#{range_start_old + 1}"
+            elsif range_end_old - range_start_old == 1
+                print "#{}{range_start_old}"
+            else
+                print "#{range_start_old},#{range_end_old - range_start_old}"
+            end
+            print " +"
+            if range_end_new - range_start_new == 0
+                print "#{range_start_new + 1}"
+            elsif range_end_new - range_start_new == 1
+                print "#{range_start_new}"
+            else
+                print "#{range_start_new},#{range_end_new - range_start_new}"
+            end
+            puts " @@"
+            while (
+                range_start_old <= range_end_old or
+                range_start_new <= range_end_new
+            )
+                if old_line_equivalent_in_new[range_start_old] == 0
+                    puts "-#{old_lines[range_start_old - 1]}"
+                    range_start_old += 1
+                elsif new_line_equivalent_in_old[range_start_new] == 0
+                    puts "+#{new_lines[range_start_new - 1]}"
+                    range_start_new += 1
+                else
+                    puts " #{old_lines[range_start_old - 1]}"
+                    range_start_old += 1
+                    range_start_new += 1
+                end
+            end
+        end
+        print "\n"
+    end
 end
