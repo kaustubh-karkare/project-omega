fdf517f677414eaf538953549f23c52a3b8fd5e7
Shivam <chotu.kv@gmail.com>
2019-08-18 20:17:33 +0530

    Do Changes

--- solutions/assignment1/library.py
+++ solutions/assignment1/library.py
@@ -1,21 +1,73 @@
-import re
+from re import search
+from sys import stderr
 
 
-class assignment1:
+class store_args_options:
 
     def __init__(self):
-       # print("class created")
+        self._all_args = {}
+        self._required_args = {}
+        self._non_required_args = {}
+        self._user_passed_args = {}
+        self._user_pass_req_args = {}
+        self._user_pass_nonreq_args = {}
+
+    def add_required_args(self, command, reqargs):
+        """ Adding new required args"""
+        self._required_args[command] = reqargs
+
+    def add_nonrequired_args(self, command, nonreqargs):
+        """ Adding new non required args"""
+        self._non_required_args[command] = nonreqargs
+
+    def get_required_args(self):
+        """ Return required args"""
+        return self._required_args
+
+    def get_non_required_args(self):
+        """ Return non-required args """
+        return self._non_required_args
+
+    def add_user_required_args(self, command, user_req_args):
+        """ Adding new user required args"""
+        self._user_pass_req_args[command] = user_req_args
+
+    def add_user_nonrequired_args(self, command, usernonreqargs):
+        """ Adding new user non-required args"""
+        self._user_pass_nonreq_args[command] = usernonreqargs
+
+    def get_user_require_args(self):
+        """ Return UserPassed Required Args"""
+        return self._user_pass_req_args
+
+    def get_user_nonrequired_args(self):
+        """ Return UserPassed non-required Args"""
+        return self._user_pass_nonreq_args
+
+    def add_all_args(self):
+        """ Get all the required and non-required args"""
+        for x in self._required_args:
+            self._all_args[x] = self._required_args[x]
+        for y in self._non_required_args:
+            self._all_args[y] = self._non_required_args[y]
+
+    def get_all_args(self):
+        """ Return all args"""
+        return self._all_args
+
+
+class parser:
+
+    def __init__(self):
+        # print("class created")
         self.copyargs = {}
-        self.requiredargs = {}
-        self.nonrequiredargs = {}
         self.checking = "noerror"
         self.userpassedargs = {}
-        self.userpassrequiredargs = {}
-        self.userpassnonrequiredargs = {}
         self.type = ""
         self.local = []
+        self.record = store_args_options()
 
-    def createoptions(self, **wargs):
+    def add_options(self, **wargs):
         """ Function for creating the command line options
 
         Args:
@@ -25,14 +77,20 @@ class assignment1:
 
         """
         # Making copy of required and nonrequiredargs
+
         self.copyargs = wargs
         for commands in self.copyargs:
             temp = self.copyargs[commands][1]
             if temp == "r":
-                self.requiredargs[commands] = self.copyargs[commands]
+                # self.requiredargs[commands] = self.copyargs[commands]
+                self.record.add_required_args(
+                    commands, self.copyargs[commands])
             else:
-                self.nonrequiredargs[commands] = self.copyargs[commands]
-        return wargs
+              #   self.nonrequiredargs[commands] = self.copyargs[commands]
+                self.record.add_nonrequired_args(
+                    commands, self.copyargs[commands])
+        # return wargs
+        self.record.add_all_args()
 
     def parsecommands(self, argslist):
         """ Function for parsing the commands
@@ -47,31 +105,40 @@ class assignment1:
             self.userpassedargs[commands[1][0]] = commands[1][1]
         # User passed non required arguments and required argumengs
         for commands in self.userpassedargs:
-            if commands in self.requiredargs:
-                self.userpassrequiredargs[commands] = self.userpassedargs[commands]
+            if commands in self.record.get_required_args():
+                self.record.add_user_required_args(
+                    commands, self.userpassedargs[commands])
             else:
-                self.userpassnonrequiredargs[commands] = self.userpassedargs[commands]
+                self.record.add_user_nonrequired_args(
+                    commands, self.userpassedargs)
 
-      #  print(self.userpassnonrequiredargs)
-        self.validateargs()
+        # print(self.userpassnonrequiredargs)
+        # return ("Nothing")
+        return self.validateargs()
 
     def validateargs(self):
         """ Function for receiving the list of commands provided by the user and validate it accordingly
 
         """
         # Checking error for mandatory and non-mandatory
-        for commands in self.requiredargs:
-            if commands not in self.userpassrequiredargs:
+
+        for commands in self.record.get_required_args():
+            if commands not in self.record.get_user_require_args():
                 self.checking = "mandatoryerror"
                 self.errorlisting(self.checking, commands)
-                exit()
+                # exit()
+                return self.checking
+
+       # print(self.record.get_required_args())
+
         for commands in self.userpassedargs:
-            if commands not in self.copyargs:
+            if commands not in self.record.get_all_args():
                 self.checking = "overflow"
                 self.errorlisting(self.checking, commands)
-                exit()
+                # exit()
+                return self.checking
 
-       # Checking for type for the argument values
+        # Checking for type for the argument values
         for commands in self.userpassedargs:
             try:
                 self.userpassedargs[commands] = int(
@@ -82,7 +149,8 @@ class assignment1:
                         self.userpassedargs[commands])
                 except ValueError:
                     try:
-                        if(self.isstring(self.userpassedargs[commands]) and isinstance(self.copyargs[commands][0],int)==False):
+                        if(self.isstring(self.userpassedargs[commands]) and isinstance(self.record.get_all_args()[commands][0], int) == False):
+
                             self.checking = "typeerrorstring"
                             self.errorlisting(self.checking, commands)
 
@@ -91,28 +159,32 @@ class assignment1:
                             self.userpassedargs[commands] = str(
                                 self.userpassedargs[commands])
                     except ValueError:
-                        exit()
+                        return self.checking
 
         # Type Checking(datatype checking)
         for commands in self.userpassedargs:
+
             typeuserpassed = type(self.userpassedargs[commands])
-            typeuserdefined = type(self.copyargs[commands][0])
+            typeuserdefined = type(self.record.get_all_args()[commands][0])
             if(typeuserdefined != typeuserpassed):
                 self.checking = "typeerror"
+                print(self.record.get_all_args()[commands])
                 self.type = typeuserdefined
                 self.errorlisting(self.checking, commands)
-                exit()
+                return self.checking
 
         for commands in self.userpassedargs:
-            self.local.append(self.copyargs[commands][2])
+            self.local.append(self.record.get_all_args()[commands][2])
 
         if "local" in self.local and "remote" in self.local:
             self.checking = "conflict"
             self.errorlisting(self.checking, "both")
-            exit()
+            return self.checking
 
         if self.checking == "noerror":
-            print(self.userpassedargs)
+            return self.userpassedargs
+        else:
+            return self.checking
 
     def errorlisting(self, errorcategory, command):
         """ Function for generating the list of errors
@@ -120,17 +192,19 @@ class assignment1:
         """
         print("Error: ")
         if errorcategory == "mandatoryerror":
-            print("The \"--%s\" argument is required, but missing from input." % command)
+            stderr.write(
+                "The \"--%s\" argument is required, but missing from input." % command)
         elif errorcategory == "overflow":
-            print("There is no \"--%s\" argument named " % command)
+            stderr.write("There is no \"--%s\" argument named " % command)
         elif errorcategory == "typeerror":
-            print("The value for the \"--%s\" argument must be a of type %s" %
-                  (command, self.type.__name__))
+            stderr.write("The value for the \"--%s\" argument must be a of type %s" %
+                         (command, self.type.__name__))
         elif errorcategory == "typeerrorstring":
-            print("The value for the \"--%s\" argument must be a of type string" %
-                  (command))
+            stderr.write("The value for the \"--%s\" argument must be a of type string" %
+                         (command))
         elif errorcategory == "conflict":
-            print("Conflicting situation found 2 mutually exclusive arguments found")
+            stderr.write(
+                "Conflicting situation found 2 mutually exclusive arguments found")
 
     def isstring(self, string):
         """ Function for checking whether a string contains number or not 
@@ -140,5 +214,4 @@ class assignment1:
         Returns
             boolean depending on string
         """
-        return bool(re.search(r'\d', string))
-
+        return bool(search(r'\d', string))
--- solutions/assignment1/test.py
+++ solutions/assignment1/test.py
@@ -1,9 +1,35 @@
 import library
+import unittest
 import sys
-if __name__ == '__main__':
-    printer = library.assignment1()
-    ans = printer.createoptions(
-        key=(1234, "r", "local"), name=("shivam", "nr", "local"))
-    commands = sys.argv[1:]
-    printer.parsecommands(commands)
 
+
+class simple_test(unittest.TestCase):
+    def setUp(self):
+        self.printer = library.parser()
+        self.printer.add_options(key=(1234, "r", "local"))
+        self.printer.add_options(name=("Shivam", "nr", "local"))
+        self.commands = ['--key=123', '--name=s']
+        self.test = self.printer.parsecommands(self.commands)
+        pass
+
+    def test_print(self):
+        self.assertEqual(self.test, {'key': 123, 'name': 's'})
+
+    def test_required_args(self):
+        self.assertEqual(self.test, {'key': 123, 'name': 's'})
+
+    def test_typeof_args(self):
+        self.commands = ['--key=123.7', '--name=s']
+        self.assertEqual(self.printer.parsecommands(
+            self.commands), {'key': 123, 'name': 's'})
+
+
+if __name__ == '__main__':
+    unittest.main()
+    """
+    printer = library.parser()
+    printer.add_options(key=(1234, "r", "local"))
+    printer.add_options(name=("shivam", "nr", "local"))
+    commands = ['--key=123', '--name=s']
+    printer.parsecommands(commands)
+    """
