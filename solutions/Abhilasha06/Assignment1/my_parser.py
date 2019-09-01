0bbad54919a8c7bca22b0e7f3944740b3b327cca
Abhilasha <abhisinha662000@gmail.com>
2019-09-01 19:01:56 +0530

    Made suggested changes.

--- solutions/Abhilasha06/Assignment1/my_parser.py
+++ solutions/Abhilasha06/Assignment1/my_parser.py
@@ -4,91 +4,145 @@
 Created on Sun Aug 25 15:21:34 2019
 @author: Abhilasha
 """
+class Error(Exception):
+
+    """
+    Error class
+    """
+
+    def __init__(self, message):
+        super(Error, self).__init__(message)
+        
+        
+class Option(object):
+
+    """
+    filed_name - name of the fields
+    dtype - type that tese fields store
+    nargs - number of arguments given for the fields
+    """
+
+    def __init__(self,  field_name, dtype, nargs):
+        self.field_name = field_name
+        self.dtype = dtype
+        self.nargs = nargs
+
+       
 class MyParser:
     """
     command line parser
     """
-    @classmethod
-    def add_option(cls, option_name, option_type, reqirement):
-        """
-        adding the options
-        """
-        list_of_fields = []
-        list_of_fields.append(option_name)
-    @classmethod
-    def check_key(cls, size, value, data):
+    def __init__(self):
+        self.options=[]
+        self.data={}
+        
+
+    def add_option(self, field_name, dtype, nargs):
+        option_being_added = Option(field_name, dtype, nargs)
+        self.options.append(option_being_added)
+        
+    
+    def check_key(self, field_type, given_type, field_size, given_size):
         """
         to check if the key is valid or not
         """
+        
         import re
-        if "--key" in data:
-            mssg = "Error: --key can't occur more than once"
-        elif size != 2:
-            mssg = "Error: --key can't be empty"
-        elif bool(re.match('^[0-9]+$', value)):
-            data["--key"] = value
-            mssg = "successful"
+        if field_size != given_size:
+            raise Error("invalid number of arguments for --key")
+            
+        elif bool(re.match('^[0-9]+$', given_type)):
+            self.data["--key"] = given_type
+            
+        elif bool(re.match('^[0-9]+$', given_type)) == False:
+            raise Error("The --key argument has invalid value.")
+            
+            
+    def check_name(self, field_type, given_type, field_size, given_size):
+        """
+        to check if the key is valid or not
+        """
+        
+        if field_size != given_size:
+            raise Error("invalid number of arguments for --name")
+            
+        elif given_type.isalpha() == False:
+            raise Error("The --name argument has invalid value.")
+            
         else:
-            mssg = "Error: The value for the '--key' argument must be a positive integer."
-        return mssg
-    @classmethod
-    def check_local(cls, size, data):
+            self.data["--name"] = given_type
+      
+    
+    def check_local(self, field_type, given_type, field_size, given_size):
         """
         to check if local is valid or not
         """
-        if size != 1:
-            mssg1 = "Error: --local takes no value"
-        elif "--remote" in data:
-            mssg1 = "Error: --local and --remote can't occur together"
+        
+        if field_size != given_size:
+            raise Error("Invalid number of arguments for --local")
+            
+        elif given_type.isalpha() == False:
+            raise Error("The --local argument has invalid value.")
+            
         else:
-            data["--local"] = "true"
-            mssg1 = "successful"
-        return mssg1
-    @classmethod
-    def check_remote(cls, size, data):
+            self.data["local"] = given_type
+        
+    
+    def check_remote(self, field_type, given_type, field_size, given_size):
         """
         to check if remote is valid or not
         """
-        if size != 1:
-            mssg2 = "Error: --remote takes no value"
-        elif "--local" in data:
-            mssg2 = "Error: --local and --remote can't occur together"
+        if field_size != given_size:
+            raise Error("Invalid number of arguments for --remote")
+            
+        elif given_type.isalpha() == False:
+            raise Error("The --remote argument has invalid value.")
+            
         else:
-            data["--remote"] = "true"
-            mssg2 = "successful"
-        return mssg2
-    @classmethod
-    def check_argument(cls, argument):
+            self.data["remote"] = given_type
+            
+            
+    def check_argument(self, argument):
         """
         to parse the given input and give error if it is invalid
         """
         import json
-        data = {}
-        list_of_fields = ["--key", "--local", "--remote", "--name"]
         argument = list(argument.split(" "))
-        no_of_args = (int)(format(len(argument)))
+        no_of_args = len(argument)
+
+        
         if no_of_args == 1:
-            ans = "Error: The '--key' argument is required, but missing from input."
+            raise Error("No arguments.")
+            
         for args in argument[1:]:
             splitted_word = args.split('=')
-            size = int(len(splitted_word))
-            if size > 2:
-                ans = "Error: Invalid argument"
-            elif splitted_word[0] not in list_of_fields:
-                return"Error: Field not defined"
-            elif splitted_word[0] == "--key":
-                ans = cls.check_key(size, splitted_word[1], data)
-            elif splitted_word[0] == "--name":
-                if size != 2:
-                    ans = "Error: --name can't be empty"
-                else:
-                    data["--name"] = splitted_word[1]
-            elif splitted_word[0] == "--local":
-                ans = cls.check_local(size, data)
-            else:
-                ans = cls.check_remote(size, data)
-        if "--key" not in data:
-            ans = "Error: The '--key' argument is required, but missing or has invalid value."
-        elif ans == "successful":
-            ans = json.dumps(data)
-        return ans
+            size = len(splitted_word)
+            flag=0
+            
+            for opt in self.options:
+            
+                if (splitted_word[0] == opt.field_name) :
+                    flag=1
+                    if(splitted_word[0] == "--key"):
+                        self.check_key(opt.field_name, splitted_word[1], opt.nargs, size)
+                        
+                    elif(splitted_word[0] == "--name"):
+                        self.check_name(opt.field_name, splitted_word[1], opt.nargs, size)
+                        
+                    elif(splitted_word[0] == "--local"):
+                        if opt.nargs == size and size == 2:
+                            self.check_key(opt.field_name, splitted_word[1], opt.nargs, size)
+                        elif opt.nargs == size and size == 1:
+                            self.data["--local"] = "true"
+                            
+                    elif(splitted_word[0]=="--remote"):
+                        if opt.nargs == size and size == 2:
+                            self.check_key(opt.dtype, splitted_word[1], opt.nargs, size)
+                        elif size == 1:
+                            self.data["--remote"] = "true"
+                            
+            if flag==0:
+                raise Error("Invalid field given.")
+          
+        return(json.dumps(self.data))
+     
--- solutions/Abhilasha06/Assignment1/test.py
+++ solutions/Abhilasha06/Assignment1/test.py
@@ -7,37 +7,65 @@ class Tests(unittest.TestCase):
     """
     some test cases
     """
-    MyParser.add_option('--key', 'int', 'True')
-    MyParser.add_option('--name', 'str', 'True')
-    MyParser.add_option('--local', 'str', 'False')
-    MyParser.add_option('--remote', 'str', 'False')
+    
+    def setUp(self):
+        self.st=MyParser()
+      
     def test_only_key_defined(self):
-        ans = MyParser.check_argument("./test --key=123")
-        self.assertEqual('{"--key": "123"}', ans)
+        self.st.add_option('--key', dtype='int', nargs=2)
+        ans = self.st.check_argument("./test --key=123")
+        self.assertEqual('{"--key": "123"}', str(ans))
+             
     def test_key_and_name_defined(self):
-        ans = MyParser.check_argument("./test --key=12345 --name=kaustubh")
-        self.assertEqual('{"--key": "12345", "--name": "kaustubh"}', ans)
+        self.st.add_option('--key', dtype='int', nargs = 2)
+        self.st.add_option('--name', dtype='str', nargs = 2)
+        ans = self.st.check_argument("./test --key=12345 --name=kaustubh")
+        self.assertEqual('{"--key": "12345", "--name": "kaustubh"}', str(ans))
+        
     def test_invalid_key(self):
-        ans = MyParser.check_argument("./test --key=cat")
-        self.assertEqual('Error: The \'--key\' argument is required,'
-                         +' but missing or has invalid value.', ans)
-    def test_no_key(self):
-        ans = MyParser.check_argument("./test --local --remote")
-        self.assertEqual('Error: The \'--key\' argument is required,'
-                         ' but missing or has invalid value.', ans)
-    def test_no_key_with_local_and_name(self):
-        ans = MyParser.check_argument("./test --local --name=abcd")
-        self.assertEqual('Error: The \'--key\' argument is required,'
-                         ' but missing or has invalid value.', ans)
+        self.st.add_option('--key', dtype='int', nargs = 2)
+        with self.assertRaises(Exception) as context:
+            self.st.check_argument("./test --key=cat")
+        self.assertEqual('The --key argument has invalid value.', str(context.exception))
+        
+    def test_local_remote(self):
+        self.st.add_option('--local', dtype='str', nargs = 1)
+        with self.assertRaises(Exception) as context:
+            self.st.check_argument("./test --local --remote")
+        self.assertEqual("Invalid field given.", str(context.exception))
+        
+    def test_local_and_name(self):
+        self.st.add_option('--name', dtype='str', nargs = 2)
+        self.st.add_option('--local', dtype='str', nargs = 1)
+        ans = self.st.check_argument("./test --local --name=abcd")
+        self.assertEqual('{"--local": "true", "--name": "abcd"}', str(ans))
+        
     def test_undefined_field(self):
-        ans = MyParser.check_argument("./test --age=19")
-        self.assertEqual('Error: Field not defined', ans)
-    def test_local_remote_together(self):
-        ans = MyParser.check_argument("./test --key=19 --local --remote")
-        self.assertEqual('Error: --local and --remote can\'t occur together', ans)
+        self.st.add_option('--key', dtype='int', nargs = 2) 
+        with self.assertRaises(Exception) as context:
+            self.st.check_argument("./test --age=19")
+        self.assertEqual('Invalid field given.', str(context.exception))
+
+        
+    def test_extra_field_given(self):
+        self.st.add_option('--key', dtype='int', nargs = 2)
+        self.st.add_option('--local', dtype='str', nargs = 1)
+        with self.assertRaises(Exception) as context:
+            self.st.check_argument("./test --key=19 --local --remote")
+        self.assertEqual('Invalid field given.', str(context.exception))
+        
     def test_key_and_local_defined(self):
-        ans = MyParser.check_argument("./test --key=19 --local")
-        self.assertEqual('{"--key": "19", "--local": "true"}', ans)
+        self.st.add_option('--key', dtype='int', nargs = 2)
+        self.st.add_option('--local', dtype='str', nargs = 1)
+        ans = self.st.check_argument("./test --key=19 --local")
+        self.assertEqual('{"--key": "19", "--local": "true"}', str(ans))
+        
+    def test_no_arguments(self):
+        self.st.add_option('--key', dtype='int', nargs = 2)
+        with self.assertRaises(Exception) as context:
+            self.st.check_argument("./test")
+        self.assertEqual('No arguments.', str(context.exception))
+        
 if __name__ == '__main__':
     unittest.main()
     
