a841a1b40f809300e0a3b94fc8723ff2a6a2169a
AkashAsthana <53980026+AkashAsthana@users.noreply.github.com>
2019-09-03 00:53:23 +0530

    Update assignment1.test.js

--- solutions/akash-asthana/Assignment-1/assignment1.test.js
+++ solutions/akash-asthana/Assignment-1/assignment1.test.js
@@ -1,28 +1,40 @@
 //USED JEST FOR TESTING THE PROJECT
-const { extractArgumentName, extractArgumentValue, typeCheck } = require("./assignment1");
+const { extractArgumentName,
+    extractArgumentValue,
+    typeCheck,
+    checkIfArgumentDefined,
+    areRequiredArgumentsPresent,
+    ifArgumentsCanBeUsedTogether,
+    parseArgumentsIntoJSON } = require("./assignment1");
 
 test("Should return the name of the argument passed", () => {
     expect(extractArgumentName("--key=212")).toEqual("key");
 });
 
-test("Should return the name of the argument passed", () => {
-    expect(extractArgumentName("--name=akash")).toEqual("name");
-});
-
-
 test('Should return value of the argument passed', () => {
     expect(extractArgumentValue("--name=akash")).toEqual("akash");
 })
-
-test('Should return value of the argument passed', () => {
-    expect(extractArgumentValue("--name=2434")).toEqual("2434");
-})
-
 
 test('Should check valid type of the argument passed', () => {
     expect(typeCheck("key", "213jkbkj12")).toEqual(false)
 })
 
-test('Should check valid type of the argument passed', () => {
-    expect(typeCheck("name", "kaustubh")).toEqual(true)
-})
+test("Should check if given argument is defined", () => {
+    expect(checkIfArgumentDefined("name")).toEqual(true)
+});
+
+test("Should check if given argument is defined", () => {
+    expect(checkIfArgumentDefined("lastName")).toEqual(false);
+});
+
+test("Should check if given argument is defined", () => {
+    expect(areRequiredArgumentsPresent(["key", "name"])).toEqual(true);
+});
+
+test("Should check if given argument is defined", () => {
+    expect(ifArgumentsCanBeUsedTogether(["key", "name", "local", "remote"])).toEqual(false);
+});
+
+
+
+
