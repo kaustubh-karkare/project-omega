6a7c325de2d3f8e799dbc0ce61d0ee7d739e50d1
pranjalrai <pranjal.rai125@gmail.com>
2019-09-10 23:26:40 +0530

    added self.assertRaises

--- solutions/Pranjal Rai/src/Assignment 1/TestCommandLParser.py
+++ solutions/Pranjal Rai/src/Assignment 1/TestCommandLParser.py
@@ -6,8 +6,8 @@ class TestCommandLParser(unittest.TestCase):
 
     def test_basic_usage(self): #all valid instructions here
         parser = CLP.CommandLineParser()
-        parser.add_command('--key', format = r'\d+')
-        parser.add_command('--name', format = r'[a-zA-Z]+', required_command = '--key')
+        parser.add_command('--key', format = r'\d+', is_required = True)
+        parser.add_command('--name', format = r'[a-zA-Z]+')
         parser.add_command('--local', conflicting_command = '--remote', is_flag = True)
         parser.add_command('--remote', conflicting_command = '--local', is_flag = True)
 
@@ -29,49 +29,34 @@ class TestCommandLParser(unittest.TestCase):
 
     def test_invalid_usage(self): #all invalid instructions here
         parser = CLP.CommandLineParser()
-        parser.add_command('--key', format = r'\d+')
-        parser.add_command('--name', format = r'[a-zA-Z]+', required_command = '--key')
+        parser.add_command('--key', format = r'\d+', is_required = True)
+        parser.add_command('--name', format = r'[a-zA-Z]+')
         parser.add_command('--local', conflicting_command = '--remote', is_flag = True)
         parser.add_command('--remote', conflicting_command = '--local', is_flag = True)
 
         command_line_param = ['./test', '--key=1a3', '--name=pranjal'] #invalid key
-        try:
-            result = parser.get_arguments(command_line_param)
-            self.assertEqual(str(result), 'invalid argument to --key')
-        except Exception as exception:
-            print(exception)
+        result = parser.get_arguments(command_line_param)
+        self.assertRaises(result, 'invalid argument to --key')
 
 
         command_line_param = ['./test', '--key=123', '--name=pranjal123'] #invalid name
-        try:
-            result = parser.get_arguments(command_line_param)
-            self.assertEqual(str(result), 'invalid argument to --name')
-        except Exception as exception:
-            print(exception)
+        result = parser.get_arguments(command_line_param)
+        self.assertRaises(result, 'invalid argument to --name')
 
 
         command_line_param = ['./test', '--name=pranjal'] #missing key
-        try:
-            result = parser.get_arguments(command_line_param)
-            self.assertEqual(str(result), 'The --key argument is required, but missing from input')
-        except Exception as exception:
-            print(exception)
-
+        result = parser.get_arguments(command_line_param)
+        self.assertRaises(result, 'The --key argument is required, but missing from input')
+        
 
         command_line_param = ['./test', '--remote', '--local'] #local and remote together
-        try:
-            result = parser.get_arguments(command_line_param)
-            self.assertEqual(str(result), 'The --remote and --local arguments cannot be used together')
-        except Exception as exception:
-            print(exception)
+        result = parser.get_arguments(command_line_param)
+        self.assertRaises(result, 'The --remote and --local arguments cannot be used together')
 
 
         command_line_param = ['./test', '--version'] #unrecognized command
-        try:
-            result = parser.get_arguments(command_line_param)
-            self.assertEqual(str(result), '--version is not a recognized command')
-        except Exception as exception:
-            print(exception)
+        result = parser.get_arguments(command_line_param)
+        self.assertEqual(result, '--version is not a recognized command')
 
 
 if __name__ == '__main__':
