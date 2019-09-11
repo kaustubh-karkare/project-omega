d71d9c4649dd9320e1d4996d5c22e4bf6e7f2824
Ankur Dubey <ankurdubey521@protonmail.com>
2019-09-12 00:01:32 +0530

    Move main function to it's own class

--- solutions/ankurdubey521/Build Automation Tool/Builder/main.py
+++ solutions/ankurdubey521/Build Automation Tool/Builder/main.py
@@ -5,78 +5,87 @@ import sys
 import os
 
 
-unresolved_commands = set()
+class Builder:
+    def __init__(self):
+        self.unresolved_commands = set()
 
+    def _build_rule_handler(self, command_name, command_dir_abs, root_dir_abs, dry_run=False):
+        """Parses JSON, Resolves Dependencies and Executes Command/Do a dry run"""
 
-class CircularDependencyException(Exception):
-    pass
+        # Remove Trailing '/' from paths if present
+        if command_dir_abs.endswith('/'):
+            command_dir_abs = command_dir_abs[:-1]
+        if root_dir_abs.endswith('/'):
+            root_dir_abs = root_dir_abs[:-1]
 
+        # Mark the command's dependencies as unresolved
+        self.unresolved_commands.add((command_name, command_dir_abs))
 
-def execute(command_name, command_dir_abs, root_dir_abs, dry_run=False):
-    """Parses JSON, Resolves Dependencies and Executes Command"""
+        # Parse build.json in current directory
+        config = BuildConfig(command_dir_abs)
+        command = config.get_command(command_name)
 
-    # Remove Trailing '/' from paths if present
-    if command_dir_abs.endswith('/'):
-        command_dir_abs = command_dir_abs[:-1]
-    if root_dir_abs.endswith('/'):
-        root_dir_abs = root_dir_abs[:-1]
+        # Process Dependencies
+        try:
+            deps = command.get_dependencies()
+            if not dry_run:
+                print("Executing {} dependencies for {} in {}...".format(len(deps), command_name, command_dir_abs))
+            for dep in deps:
+                dep_dir_abs = ''
+                dep_name = ''
+                if dep.startswith('//'):
+                    # Command path referenced from root directory
+                    dep_dir_abs = root_dir_abs + '/' + dep[2:]
+                    dep_dir_abs, dep_name = dep_dir_abs.rsplit('/', 1)
+                elif '/' in dep:
+                    # Command in child directory
+                    dep_dir_abs, dep_name = dep.rsplit('/', 1)
+                    dep_dir_abs = command_dir_abs + "/" + dep_dir_abs
+                else:
+                    # Command in same directory
+                    dep_name = dep
+                    dep_dir_abs = command_dir_abs
 
-    # Mark the command's dependencies as unresolved
-    unresolved_commands.add((command_name, command_dir_abs))
+                # Check for circular dependencies
+                if (dep_name, dep_dir_abs) in self.unresolved_commands:
+                    raise self.CircularDependencyException(
+                        "Detected a Circular Dependency between {}:{} and {}:{}"
+                        .format(dep_dir_abs, dep_name, command_dir_abs, command_name))
 
-    # Parse build.json in current directory
-    config = BuildConfig(command_dir_abs)
-    command = config.get_command(command_name)
+                self._build_rule_handler(dep_name, dep_dir_abs, root_dir_abs, dry_run)
 
-    # Process Dependencies
-    try:
-        deps = command.get_dependencies()
-        print("Executing {} dependencies for {} in {}...".format(len(deps), command_name, command_dir_abs))
-        for dep in deps:
-            dep_dir_abs = ''
-            dep_name = ''
-            if dep.startswith('//'):
-                # Command path referenced from root directory
-                dep_dir_abs = root_dir_abs + '/' + dep[2:]
-                dep_dir_abs, dep_name = dep_dir_abs.rsplit('/', 1)
-            elif '/' in dep:
-                # Command in child directory
-                dep_dir_abs, dep_name = dep.rsplit('/', 1)
-                dep_dir_abs = command_dir_abs + "/" + dep_dir_abs
-            else:
-                # Command in same directory
-                dep_name = dep
-                dep_dir_abs = command_dir_abs
+        except Command.NoDependenciesException:
+            print("No dependencies found for {} in {}...".format(command_name, command_dir_abs))
 
-            # Check for circular dependencies
-            if (dep_name, dep_dir_abs) in unresolved_commands:
-                raise CircularDependencyException(
-                    "Detected a Circular Dependency between {}:{} and {}:{}"
-                    .format(dep_dir_abs, dep_name, command_dir_abs, command_name))
+        # Mark the command's dependencies as resolved
+        self.unresolved_commands.remove((command_name, command_dir_abs))
 
-            execute(dep_name, dep_dir_abs, root_dir_abs, dry_run)
+        if not dry_run:
+            # Execute Command after processing dependencies
+            print("Executing {} in {}".format(command_name, command_dir_abs))
+            return_value = run(command.get_command_string(), command_dir_abs, print_command=True)
 
-    except Command.NoDependenciesException:
-        print("No dependencies found for {} in {}...".format(command_name, command_dir_abs))
+            # Stop Execution if Command Fails
+            if return_value != 0:
+                exit(-1)
 
-    # Mark the command's dependencies as resolved
-    unresolved_commands.remove((command_name, command_dir_abs))
+    def execute_build_rule(self, command_name, command_dir_abs, root_dir_abs):
+        # Do a dry run and check for syntax, do validation and check for circular dependencies
+        self._build_rule_handler(command_name, command_dir_abs, root_dir_abs, dry_run=True)
 
-    print("Executing {} in {}".format(command_name, command_dir_abs))
-    if not dry_run:
-        # Execute Command after processing dependencies
-        return_value = run(command.get_command_string(), command_dir_abs, print_command=True)
+        # If everything is fine do an actual run
+        self._build_rule_handler(command_name, command_dir_abs, root_dir_abs)
 
-        # Stop Execution if Command Fails
-        if return_value != 0:
-            exit(-1)
+    class CircularDependencyException(Exception):
+        pass
 
 
 if __name__ == '__main__':
     command = sys.argv[1]
+    builder = Builder()
     relative_path = ''
     if '/' in command:
         # Handle relative paths
         relative_path, command = command.rsplit('/', 1)
     path = os.getcwd() + "/" + relative_path
-    execute(command, path, os.getcwd())
+    Builder.execute_build_rule(command, path, os.getcwd())
--- solutions/ankurdubey521/Build Automation Tool/Builder/tests/test_builder.py
+++ solutions/ankurdubey521/Build Automation Tool/Builder/tests/test_builder.py
@@ -1,42 +1,47 @@
 import unittest
 import subprocess
 import os
-from Builder.main import execute, CircularDependencyException
+from Builder.main import Builder
 
 
 class TestBuilder(unittest.TestCase):
 
     def test_compilation_basic(self):
+        builder = Builder()
         path = os.getcwd() + '/test_builder_files/test_compilation_basic'
-        execute('run', path, path)
+        builder.execute_build_rule('run', path, path)
         exec_path = '"' + path + '/test.out' + '"'
         result = subprocess.run(exec_path, shell=True, capture_output=True, text=True)
         self.assertEqual('1 2 3 4 5 \n1 2 3 4 5 \n1 2 3 4 5 \n', result.stdout)
-        execute('clean', path, os.getcwd())
+        builder.execute_build_rule('clean', path, os.getcwd())
 
     def test_commands_referenced_from_root(self):
+        builder = Builder()
         path = os.getcwd() + '/test_builder_files/test_commands_referenced_from_root'
-        execute('run', path, path)
+        builder.execute_build_rule('run', path, path)
         output_file_path = path + '/output'
         result = ''
         with open(output_file_path) as file_handle:
             result = file_handle.readable()
-        execute('clean', path, os.getcwd() + '/test_builder_files/test_commands_referenced_from_root')
+        builder.execute_build_rule('clean', path, os.getcwd() + '/test_builder_files/test_commands_referenced_from_root')
         self.assertEqual(True, result)
 
     def test_dry_run_does_not_write_files(self):
+        builder = Builder()
         path = os.getcwd() + '/test_builder_files/test_commands_referenced_from_root'
-        execute('run', path, path, dry_run=True)
+        builder._build_rule_handler('run', path, path, dry_run=True)
         output_file_path = path + '/output'
         self.assertEqual(False, os.path.isfile(output_file_path))
 
     def test_basic_circular_dependency_throws_exception(self):
+        builder = Builder()
         path = os.getcwd() + '/test_builder_files/test_basic_circular_dependency_throws_exception'
-        self.assertRaises(CircularDependencyException, execute, 'A', path, path, dry_run=True)
+        self.assertRaises(Builder.CircularDependencyException, builder.execute_build_rule, 'A', path, path)
 
     def test_basic_circular_dependency2_throws_exception(self):
+        builder = Builder()
         path = os.getcwd() + '/test_builder_files/test_basic_circular_dependency2_throws_exception'
-        self.assertRaises(CircularDependencyException, execute, 'A', path, path, dry_run=True)
+        self.assertRaises(Builder.CircularDependencyException, builder.execute_build_rule, 'A', path, path)
 
 
 if __name__ == '__main__':
