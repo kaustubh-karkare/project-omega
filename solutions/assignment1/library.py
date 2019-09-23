3c05b35caac8995cddac15de8d004cc32d54bd32
Shivam <chotu.kv@gmail.com>
2019-09-23 23:40:02 +0530

    Add Own Exception

--- solutions/assignment1/library.py
+++ solutions/assignment1/library.py
@@ -2,6 +2,17 @@ from re import search
 from sys import stderr
 
 
+class ParserError(Exception):
+
+    # Constructor or Initializer
+    def __init__(self, value):
+        self.value = value
+
+    # __str__ is to print() the value
+    def __str__(self):
+        return(str(self.value))
+
+
 class Parser:
 
     def __init__(self):
@@ -11,11 +22,10 @@ class Parser:
 
     def add_option(self, *args, **kwargs):
         """ Function for adding expected arguments info
-
+            add_option(name,required=true,type=float,unique_key=local)
         Args:
             **kwargs : Keyword Arguments with expected arguments information
         """
-
         for arg in args:
             self._expected_args[arg] = kwargs
 
@@ -29,10 +39,8 @@ class Parser:
             The User passed values if everything goes well , otherwise returns the error
         """
         # Storing required Args for Error Checking
-        for keys, value in self._expected_args.items():
-            for i in value:
-                if i == 'required':
-                    self._required_args[keys] = value
+        required_args = set(
+            [name for name, value in self._expected_args.items() if "required" in value])
 
         unique_key_nums = 0
         present_unique_key = ""
@@ -41,14 +49,10 @@ class Parser:
 
         for keys in argslist:
             if keys.startswith('--'):
-                x = keys[2:].partition('=')
-                name = x[0]
-                value = x[2]
+                name, _separator, value = keys[2:].partition('=')
                 if name not in self._expected_args:
-                    try:
-                        raise ValueError("unexpected")
-                    except Exception as error:
-                        return str(error)
+                    raise ParserError("unexpected")
+
                 try:
                     value = int(value)
                 except ValueError:
@@ -57,10 +61,7 @@ class Parser:
                     except ValueError:
                         try:
                             if(self.isstring(value) and isinstance(self._expected_args[name]['type'], int) == False):
-                                try:
-                                    raise ValueError("typeerrorstring")
-                                except Exception as error:
-                                    return str(error)
+                                raise TypeError("typeerrorstring")
                             else:
                                 value = str(value)
                         except ValueError:
@@ -70,26 +71,17 @@ class Parser:
                 typeuserdefined = self._expected_args[name]['type']
                 if(typeuserdefined != typeuserpassed):
                     self.checking = "typeerror"
-                    try:
-                        raise TypeError("typeerror")
-                    except Exception as error:
-                        return str(error)
+                    raise TypeError("typeerror")
 
                 if 'unique_key' in self._expected_args[name].keys():
                     previous_unique_key = present_unique_key
                     present_unique_key = self._expected_args[name]['unique_key']
                 if previous_unique_key != "" and previous_unique_key != present_unique_key:
-                    try:
-                        raise ValueError("conflict")
-                    except Exception as error:
-                        return str(error)
+                    raise ParserError("conflict")
 
-        for commands in self._required_args:
+        for commands in required_args:
             if commands not in user_passed_args:
-                try:
-                    raise ValueError("mandatoryerror")
-                except Exception as error:
-                    return str(error)
+                raise ParserError("mandatoryerror")
         return user_passed_args
 
     def isstring(self, string):
--- solutions/assignment1/test.py
+++ solutions/assignment1/test.py
@@ -9,8 +9,8 @@ class SimpleTest(unittest.TestCase):
         self.parser.add_option('key', required=True,
                                type=int, unique_key="local")
         self.parser.add_option('name',  type=str, unique_key="remote")
-        self.parser.add_option('key2', mode="local", type=float)
-        self.parser.add_option('name1', mode="local", type=str)
+        self.parser.add_option('key2', unique_key="local", type=float)
+        self.parser.add_option('name1', unique_key="local", type=str)
         pass
 
     def test_print(self):
@@ -22,30 +22,51 @@ class SimpleTest(unittest.TestCase):
         self.commands = ['--key=123']
         self.assertEqual(self.parser.parse(self.commands), {'key': 123})
         self.commands = ['--key2=123.6']
-        self.assertEqual(self.parser.parse(self.commands), 'mandatoryerror')
+        try:
+            self.parser.parse(self.commands)
+        except Exception as error:
+            self.assertEqual(str(error), 'mandatoryerror')
 
     def test_overflow_args(self):
         self.commands = ['--key=1234']
         self.assertEqual(self.parser.parse(self.commands), {'key': 1234})
         self.commands = ['--key3=123', '--key=123']
-        self.assertEqual(self.parser.parse(self.commands), 'unexpected')
+        try:
+            self.parser.parse(self.commands)
+        except Exception as error:
+            self.assertEqual(str(error), 'unexpected')
 
     def test_typeof_args(self):
         self.commands = ['--key=123.6']
-        self.assertEqual(self.parser.parse(self.commands), 'typeerror')
+        try:
+            self.parser.parse(self.commands)
+        except Exception as error:
+            self.assertEqual(str(error), 'typeerror')
         self.commands = ['--key=1234s']
-        self.assertEqual(self.parser.parse(self.commands), 'typeerrorstring')
+        try:
+            self.parser.parse(self.commands)
+        except Exception as error:
+            self.assertEqual(str(error), 'typeerrorstring')
         self.commands = ['--key=123', '--name1=Shiva12']
-        self.assertEqual(self.parser.parse(self.commands), 'typeerrorstring')
+        try:
+            self.parser.parse(self.commands)
+        except Exception as error:
+            self.assertEqual(str(error), 'typeerrorstring')
         self.commands = ['--key=123', '--name1=Shivam', '--key2=123']
-        self.assertEqual(self.parser.parse(self.commands), 'typeerror')
+        try:
+            self.parser.parse(self.commands)
+        except Exception as error:
+            self.assertEqual(str(error), 'typeerror')
 
     def test_conflict(self):
         self.commands = ['--key=123', '--key2=1234.6', '--name1=Shivam']
         self.assertEqual(self.parser.parse(self.commands),
                          {'key': 123,  'key2': 1234.6, 'name1': 'Shivam'})
         self.commands = ['--key=123', '--name=Shiva']
-        self.assertEqual(self.parser.parse(self.commands), 'conflict')
+        try:
+            self.parser.parse(self.commands)
+        except Exception as error:
+            self.assertEqual(error.value, 'conflict')
 
 
 if __name__ == '__main__':
