7df0d4a63ade3a444073ff04a9ef6f9ce05a8c4e
Ankur Dubey <ankurdubey521@protonmail.com>
2019-10-03 21:42:27 +0530

    Integrate File watching in Build System

--- solutions/ankurdubey521/Build Automation Tool/Builder/main.py
+++ solutions/ankurdubey521/Build Automation Tool/Builder/main.py
@@ -1,4 +1,6 @@
 from Builder.lib.builder import Builder
+from Builder.lib.buildconfig import BuildConfig
+from Builder.lib.file_watcher import FileWatcher
 import argparse
 import sys
 import os
@@ -8,7 +10,7 @@ if __name__ == '__main__':
     # Parse Args
     parser = argparse.ArgumentParser()
     parser.add_argument('build_rule', type=str)
-    parser.add_argument('--watch', type=bool)
+    parser.add_argument('--watch', action="store_true")
     args = parser.parse_args(sys.argv[1:])
 
     # Parse Command
@@ -23,11 +25,16 @@ if __name__ == '__main__':
     # TODO: Make root directory configurable
 
     try:
-        if args.watch is None:
+        if not args.watch:
             builder.execute_build_rule(command, path, os.getcwd())
         else:
             # Watch files and run build rule on changes
-            pass
+            file_watcher = FileWatcher()
+            file_list = BuildConfig(path).get_command(command).get_files()
+            # Convert file paths to absolute
+            file_list = [(path + file) for file in file_list]
+            file_watcher.watch_and_execute(file_list, builder.execute_build_rule, command, path, os.getcwd())
+
     except Exception as e:
-        print(e)
+        print(e.with_traceback())
 
