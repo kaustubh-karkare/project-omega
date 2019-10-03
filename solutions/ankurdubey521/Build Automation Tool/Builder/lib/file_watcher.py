c421aa696b8dc8f03bbdbe76e73d5331acb19dfb
Ankur Dubey <ankurdubey521@protonmail.com>
2019-10-03 21:22:33 +0530

    Implement class for listening changes to files

---
+++ solutions/ankurdubey521/Build Automation Tool/Builder/lib/file_watcher.py
@@ -0,0 +1,31 @@
+from os import stat
+from typing import List, Callable, Dict
+from time import sleep
+
+
+class FileWatcher:
+    FILE_WATCH_INTERVAL_SECONDS = 1
+
+    def _get_file_edit_times(self, file_list: List[str]) -> Dict[str, int]:
+        """Populate a Dict of last edit times of files and return it"""
+        file_edit_times = {}
+        for file in file_list:
+            file_edit_times[file] = stat(file).st_mtime
+        return file_edit_times
+
+    def watch_and_execute(self, file_list: List[str], function: Callable[[any], any], *args: any) -> None:
+        """Execute a function whenever a file from file_list changes"""
+        print("Listening for changes on {}...".format(file_list))
+        file_edit_times = self._get_file_edit_times(file_list)
+        print("Executing command for the first time...")
+        function(*args)
+        while True:
+            sleep(FileWatcher.FILE_WATCH_INTERVAL_SECONDS)
+            for file in file_list:
+                new_file_edit_times = self._get_file_edit_times(file_list)
+                if new_file_edit_times != file_edit_times:
+                    file_edit_times = new_file_edit_times
+                    print("Detected change of file. Executing command...")
+                    function(*args)
+
+
---
+++ solutions/ankurdubey521/Build Automation Tool/Builder/tests/test_file_watcher.py
@@ -0,0 +1,52 @@
+import unittest
+from Builder.lib.file_watcher import FileWatcher
+import os
+from time import sleep
+from multiprocessing import Process
+
+def copy_file(input_file_path: str, output_file_path: str) -> None:
+    contents = ''
+    with open(input_file_path, 'r') as file:
+        contents = file.read()
+    with open(output_file_path, 'w') as file:
+        file.write(contents)
+
+
+class MyTestCase(unittest.TestCase):
+    # The tests should work for any path inside the project
+    def setUp(self):
+        while os.path.basename(os.getcwd()) != 'Build Automation Tool':
+            os.chdir('..')
+        os.chdir('Builder/tests')
+
+    def test_copy_file_on_file_change(self):
+        # Initialize Files
+        file_path = os.getcwd() + "/test_file_watcher_files/test_copy_file_on_file_change/"
+        input_file = file_path + "input.txt"
+        output_file = file_path + "output.txt"
+        with open(input_file, 'w') as file:
+            file.write("Hello World 1.0")
+        with open(output_file, 'w') as file:
+            file.write("Hello World 1.0")
+
+        # Activate Watcher and change tracked file
+        file_watcher = FileWatcher()
+        process = Process(target=file_watcher.watch_and_execute, args=([input_file], copy_file, input_file, output_file))
+        process.start()
+        # file_watcher.watch_and_execute([input_file], copy_file, input_file, output_file)
+        with open(input_file, 'w') as file:
+            file.write("Hello World 2.0")
+        sleep(2)
+        output_file_content = ""
+        with open(output_file, 'r') as file:
+            output_file_content = file.read()
+        self.assertEqual("Hello World 2.0", output_file_content)
+
+        # Cleanup
+        with open(input_file, 'w') as file:
+            file.write("Hello World 1.0")
+        os.remove(output_file)
+        process.terminate()
+
+if __name__ == '__main__':
+    unittest.main()
