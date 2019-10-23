ced1090208a729f87f1df4f3c85b8cb57ccc5708
Shivam <chotu.kv@gmail.com>
2019-10-23 22:20:07 +0530

    Do Changes

--- solutions/assignment1/library.py
+++ solutions/assignment1/library.py
@@ -19,14 +19,19 @@ class Parser:
         # print("class created")
         self._expected_args = {}
 
-    def add_option(self, *args, **kwargs):
+    def add_option(self, name, *, required=False, type=str, unique_key=None):
         """ Function for adding expected arguments info
-            add_option(name,required=true,type=float,unique_key=local)
         Args:
-            **kwargs : Keyword Arguments with expected arguments information
+            required : Mark it true if any argument is true
+            type : type for the argument passed 
+            unique_key : unique_key value for no mutually exclusive things
         """
-        for arg in args:
-            self._expected_args[arg] = kwargs
+        user_passed_arg = {
+            "required": required,
+            "type": type,
+            "unique_key": unique_key
+        }
+        self._expected_args[name] = user_passed_arg
 
     def parse(self, argslist):
         """ Function for parsing and validating the commands
@@ -39,12 +44,14 @@ class Parser:
         """
         # Storing required Args for Error Checking
         required_args = set(
-            [name for name, value in self._expected_args.items() if "required" in value])
+            [name for name, value in self._expected_args.items() for x1, x2 in value.items() if x2 == True])
 
         unique_key_nums = 0
         present_unique_key = ""
         previous_unique_key = ""
         user_passed_args = {}
+        conflict_checker = {}
+        uniquekey_count = 0
 
         for key in argslist:
             if key.startswith('--'):
@@ -52,42 +59,27 @@ class Parser:
                 if name not in self._expected_args:
                     raise ParserError("unexpected value", name)
 
-                typeuserdefined = self._expected_args[name]['type']
-                if typeuserdefined == int:
-                    try:
-                        value = int(value)
-                    except ValueError:
-                        raise ParserError("typeerror")
-                if typeuserdefined == float:
-                    try:
-                        value = float(value)
-                    except ValueError:
-                        raise ParserError("typeerror")
-                typeuserpassed = type(value)
+                try:
+                    value = self._expected_args[name]['type'](value)
+                except ValueError:
+                    raise ParserError("typeerror")
                 user_passed_args[name] = value
 
-                if(typeuserdefined != typeuserpassed):
-                    self.checking = "typeerror"
-                    raise ParserError("typeerror")
+                uniquekey = self._expected_args[name]['unique_key']
 
-                if 'unique_key' in self._expected_args[name].keys():
-                    previous_unique_key = present_unique_key
-                    present_unique_key = self._expected_args[name]['unique_key']
-                if previous_unique_key != "" and previous_unique_key != present_unique_key:
+                if uniquekey is not None:
+                    if uniquekey in conflict_checker:
+                        conflict_checker[uniquekey] = 2
+                    else:
+                        conflict_checker[uniquekey] = 1
+                        uniquekey_count = uniquekey_count + 1
+
+                if uniquekey_count > 1:
                     raise ParserError("conflict")
             else:
                 raise ParserError("Unexpected Format")
+
         for commands in required_args:
             if commands not in user_passed_args:
                 raise ParserError("mandatoryerror")
         return user_passed_args
-
-    def isstring(self, string):
-        """ Function for checking whether a string contains number or not 
-
-        Args:
-            string: String to be checked
-        Returns
-            boolean depending on string
-        """
-        return bool(search(r'\d', string))
