47ad59587b08fe40515420596218078cff6c5c7c
D R Siddhartha <siddharthadr11@gmail.com>
2019-10-28 14:34:23 +0530

    resolved changes

--- solutions/assignment-1/CLIparser.py
+++ solutions/assignment-1/CLIparser.py
@@ -4,70 +4,64 @@ Command Line Parser - parses command line arguments
 
 import sys
 
-class Argument(object):
-    """
-    Class to define argument definitions and characteristics
-    """
-
-    def __init__(self, arg_name, arg_type, cant_be_used_with=[], required=False):
-        self.arg_name = arg_name
-        self.arg_type = arg_type
-        self.value = None
-        self.required = required
-        self.used = False           #flag to identify if the argument is used or not
-        self.cant_be_used_with = cant_be_used_with
-
 class Parser(object):
     """
     Class to handle parsing
     """
 
-    def __init__(self):
-        self.args = {}          #storing arguments as objects as values and their names as keys
+    def __init__(self, test=False):
+        self.args = {}		#storing arguments as objects as values and their names as keys
+        self.test = test
 
-    def add_argument(self, arg_name, arg_type, cant_be_used_with=[], required=False):
+    def add_argument(self, arg_name, **kwargs):
         """
         Initializes Argument class
         """
-        self.args[arg_name] = Argument(arg_name, arg_type, cant_be_used_with, required)
+        self.args[arg_name] = dict()
+        self.args[arg_name]["used"] = False
+        self.args[arg_name]["cant_be_used_with"] = None
+        self.args[arg_name]["required"] = False
+		
+        for name, value in kwargs.items():
+            self.args[arg_name][name] = value
+            
 
-
-    def parse_arguments(self):
+    def parse_arguments(self, *args):
         """
         Parses argument and assignes value to argument dictionary
         """
-        arguments = sys.argv[1:]
+        if self.test:
+            arguments = args
+        else:
+            arguments = sys.argv[1:]
 
         for argument in arguments:
+            name, sign, value = argument.partition('=')
+            name = name.split("-")[-1]
             try:
-                arg, value = argument.split('=')
-                value = int(value) if value.isdigit() else value        #typecasting integer value if string
+                value = int(value) if (self.args[name]["type"] == int) else value        #typecasting
+                value = float(value) if (self.args[name]["type"] == float) else value	   
             except ValueError:
-                arg = argument
-                value = None
-            arg = arg[2:]           #removing the dashes
+                pass
+            if value:                
+                if not isinstance(value, self.args[name]["type"]):
+                    raise WrongTypeError(type(value), self.args[name]["type"], name)
+            if not name in self.args:
+                raise NoSuchArgError(name)
 
-            if value:
-                if not arg in self.args:
-                    raise NoSuchArgError(arg)
-                if not isinstance(value, self.args[arg].arg_type):
-                    raise WrongTypeError(type(value), self.args[arg].arg_type, arg)
-            else:
-                if not arg in self.args:
-                    raise NoSuchArgError(arg)
-
-            self.args[arg].value = value
-            self.args[arg].used = True
+            self.args[name]["value"] = value
+            self.args[name]["used"] = True
 
         for arg in self.args:       #checking if an argument can be used with the others
-            if self.args[arg].used:
-                for other_arg in self.args[arg].cant_be_used_with:
-                    if self.args[other_arg].used:
-                        raise SimultaneousUsageError(arg, self.args[arg].cant_be_used_with)
+            if self.args[arg]["used"]:
+                if self.args[arg]["cant_be_used_with"]:
+                    for other_arg in self.args[arg]["cant_be_used_with"]:
+                        if self.args[other_arg]["used"]:
+                            raise SimultaneousUsageError(arg, self.args[arg]["cant_be_used_with"])
 
         for argument in self.args:      #checking if the required argument is being used or not
-            if self.args[argument].required:
-                if not self.args[argument].used:
+            if self.args[argument]["required"]:
+                if not self.args[argument]["used"]:
                     raise ReqArgError(argument)
 
 class NoSuchArgError(Exception):
