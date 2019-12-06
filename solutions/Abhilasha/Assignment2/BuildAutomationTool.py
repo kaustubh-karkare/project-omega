ea843428c74642e75b273db2dbb3056fd3340722
Abhilasha <abhisinha662000@gmail.com>
2019-12-06 18:34:33 +0530

    Update BuildAutomationTool.py

--- solutions/Abhilasha/Assignment2/BuildAutomationTool.py
+++ solutions/Abhilasha/Assignment2/BuildAutomationTool.py
@@ -1,117 +1,165 @@
 import os
+
 import json
-import subprocess
+
 import threading
-import time
+
 
 class Action():
+
     
     def get_command(self, cwd, command, action):
-        
+
+
         if command != "build":
-            
+
             raise Exception('Command not recognized.')
+
             return
         
-        else:
-            
+
+        else:            
+
             self.get_action(cwd, action)
+
             return
-        
-        
+
+                
+
     def get_action(self, cwd, action):
         
+
         os.chdir(cwd)
+
         action_found = False
 
+
         with open(os.path.join("build.json")) as json_file:
-            
+        
             all_text = json.load(json_file)            
-            for text in all_text:
-                
+
+            for text in all_text:                
+
                 if text['name'] == action:
                     
+                    order = 0
+                    
                     action_found = True
+
                     ActionGraph_obj = ActionGraph()
-                    ActionGraph_obj.plot_graph(text, cwd)
+
+                    ActionGraph_obj.plot_graph(text, cwd, order)
+
                     ActionGraph_obj.execute_commands()
 
+
             if action_found is False:
-                
+
                 raise Exception('Command not recognized.')
+
                 return
 
+
         return
-    
+
     
 class ActionGraph():
     
+
      def __init__(self):
+
          
         self.commands = []
+
         self.working_directories = []
-    
-    
-     def plot_graph(self, text, cwd):
         
+        self.orders = []
+        
+  
+
+     def plot_graph(self, text, cwd, order):
+        
+
         if 'deps' in text:
-            
+
             for dependency in text['deps']:
-                
+
                 if len(str(dependency).split('/')) == 2:
-                    
+
                     dependency = str(dependency).split('/')[1]
-                    with open(os.path.join("algorithms\\build.json")) as json_file:
-                        
+
+                    with open(os.path.join("algorithms//build.json")) as json_file:
+
                         all_text_new = json.load(json_file)                      
-                        for text_new in all_text_new:
-                            
+
+                        for text_new in all_text_new:                            
+
                             if text_new['name'] == dependency:
                                 
-                                self.plot_graph(text_new, cwd+"\\algorithms")
-                                
+                                self.plot_graph(text_new, cwd+"\\algorithms", order+1)                                
+
                 else:
-                    
+                
                     with open(os.path.join("build.json")) as json_file:
-                        
+                    
                         all_text_new = json.load(json_file)
+
                         for text_new in all_text_new:
-                            
+        
                             if text_new['name'] == dependency:
                                 
-                                self.plot_graph(text_new, cwd)
+                                self.plot_graph(text_new, cwd, order+1)
         
-        if 'command' in text:
-                
-            if text['command'] not in self.commands:
-                
+
+        if 'command' in text:                
+
+            if text['command'] not in self.commands:                
+
                 self.commands.append(text['command'])
+
                 self.working_directories.append(cwd)
-                                
+                
+                self.orders.append(order)
+
         return
+    
     
      def process(self, command, work_dir):
          
         os.chdir(work_dir)
+
         os.system(command)
-        return
-            
-        
-     def execute_commands(self):
-       
-        thread_list = []
-        count = 0
-        for command, work_dir in zip(self.commands, self.working_directories):
-         
-            thread = threading.Thread(target = self.process, args = (command, work_dir))
-            thread_list.append(thread)
-            time.sleep(2)
-            thread_list[count].start()
-            count += 1
-      
-        for thread in thread_list:
-            
-            thread.join()   
-            
+
         return
 
+            
+     def execute_commands(self):
+         
+        
+        order_count = max(self.orders) 
+        
+        for i in range(order_count+1):
+            
+            thread_list = []
+
+            count = 0
+
+            for command, work_dir, order in zip(self.commands, self.working_directories, self.orders):
+
+                if order == i:
+                    
+                    thread = threading.Thread(target = self.process, args = (command, work_dir))
+
+                    thread_list.append(thread)
+
+                    thread_list[count].start()
+
+                    count += 1
+                    
+     
+            for thread in thread_list:
+
+                thread.join()   
+
+        return
+    
