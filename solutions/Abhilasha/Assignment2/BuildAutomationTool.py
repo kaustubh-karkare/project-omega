799c02ff1682b88d7cd4a0d32cd76f97a806ef42
Abhilasha <abhisinha662000@gmail.com>
2019-12-28 01:46:52 +0530

    Create separate Action and ActionExecutor classes.

--- solutions/Abhilasha/Assignment2/BuildAutomationTool.py
+++ solutions/Abhilasha/Assignment2/BuildAutomationTool.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+
 """
 Created on Fri Dec  6 14:36:59 2019
-
 @author: Abhilasha
+
 """
 
 import os
@@ -11,148 +12,174 @@ import subprocess
 from pathlib import Path
 from os.path import relpath
 import time
- 
+from enum import Enum
 
-class ActionGraph():
+
+class Status(Enum):
     
-    '''loads commands, dependencies and initialises the status for all the actions from all build.json files.'''
-    
-    def __init__(self, root_dir):
-        
-        self.root_dir = root_dir
-        self.all_action_status = {}
+    not_started = 1
+    ready = 2
+    processing = 3
+    done = 4
+
+
+class ActionGraph():    
+
+    '''loads commands, dependencies and initialises the status for all the actions from all build.json files.'''    
+
+    def __init__(self):
+
         self.all_action_commands = {}
         self.all_action_dependencies = {}
+
         
-    def create_action_map(self):
-        
-        for filename in Path(self.root_dir).rglob('*.json'):
-            
-            if str(filename).endswith('\\build.json'):
-                path_elements = str(relpath(filename, self.root_dir)).split(os.path.sep)          
-                rel_location = os.path.sep.join(path_elements[:-1])
-                with open(filename) as json_file:
-                    all_data = json.load(json_file)
-                for data in all_data:
-                    if rel_location != '':
-                        action = rel_location+'/'+data['name']
-                    else:
-                        action = data['name']
-                    self.all_action_status[action] = 'not started'
-                    self.all_action_commands[action] = data.get('command')
-                    self.all_action_dependencies[action] = data.get('deps')  
-        return
+    def create_action_map(self, root_dir):
+
+        for filename in Path(root_dir).rglob('build.json'):           
+            path_elements = str(relpath(filename, root_dir)).split(os.path.sep)          
+            rel_location = os.path.sep.join(path_elements[:-1])
+            with open(filename) as json_file:
+                all_data = json.load(json_file)
+            for data in all_data:
+                if rel_location != '':
+                    action = rel_location+'/'+data['name']
+                else:
+                    action = data['name']
+                self.all_action_commands[action] = data.get('command')
+                self.all_action_dependencies[action] = data.get('deps')  
+        return self.all_action_commands, self.all_action_dependencies
 
             
-    def get_command_to_be_executed(self, build, action):
-        
-        if build != 'build':
-            raise Exception('Command not recognized.')
-            
-        if action not in self.all_action_status:
-            raise Exception('Command not recognized.')
-                  
-        
-        Action_obj = ActionExecutor(self.all_action_status, self.all_action_commands, self.all_action_dependencies)
-        Action_obj.get_dependencies(action)
-        Action_obj.store_dependents_for_all_action()
-        Action_obj.initialize_dependencies_status(action)
-        Action_obj.initialize_pending_actions()
-        Action_obj.execute_commands()
-        return
-                    
-              
-class ActionExecutor():
+class Action():
     
+    def __init__(self):
 
-    def __init__(self, all_action_status, all_action_commands, all_action_dependencies):
-     
-        self.all_action_status = all_action_status
-        self.all_action_commands = all_action_commands
-        self.all_action_dependencies = all_action_dependencies
         self.current_action_status = {}
         self.current_action_commands = {}
         self.current_action_dependencies = {}
         self.dependents = {}
-        self.ongoing_subprocesses = []
-        self.action_name_for_ongoing_subprocess = []
-        self.action_name = ''
-        self.pending_actions = []
         
+    
+    def action_sequence(self, root, build, action):
+            
+        ActionGraph_obj = ActionGraph()
+        self.all_action_commands, self.all_action_dependencies = ActionGraph_obj.create_action_map(root)
         
-    def initialize_pending_actions(self):
-        for action in self.current_action_status:
-            self.pending_actions.append(action)
-        return
+        if action not in self.all_action_commands:
+            raise Exception('Command not recognized.')
+            
+        self.get_dependencies(action)
+        self.store_dependents_for_all_action()
+        self.initialize_dependencies_status(action)
         
-        
-    def get_dependencies(self, action):
-         
-        '''stores the status, commands, dependencies of the action to be executed and all its dependencies'''
-        
-        self.current_action_status[action] = self.all_action_status[action]
+        return self.current_action_status, self.current_action_commands, self.current_action_dependencies, self.dependents
+    
+
+    def get_dependencies(self, action):         
+
+        '''stores the status, commands, dependencies of the action to be executed and all its dependencies'''       
+
+        self.current_action_status[action] = Status.not_started
         self.current_action_commands[action] = self.all_action_commands[action]
         self.current_action_dependencies[action] = self.all_action_dependencies[action]    
+
         if not self.all_action_dependencies[action] is None:
             for name in self.all_action_dependencies[action]:
                 self.get_dependencies(name)
         return
-    
-                   
+
+
     def store_dependents_for_all_action(self):
-         
+
         for action in self.current_action_status:
-            self.dependents.setdefault(action, [])
-               
+            self.dependents.setdefault(action, [])               
+
         for action in self.current_action_status:
             self.action_name = action
             self.get_dependents(action)
+
         return
-                       
-            
-    def get_dependents(self, action):
-         
+    
+
+    def get_dependents(self, action):  
+
          if not self.all_action_dependencies[action] is None:                 
             for dep in self.all_action_dependencies[action]:
                 if self.action_name not in self.dependents[dep]:
-                    self.dependents[dep].append(self.action_name)                               
+                    self.dependents[dep].append(self.action_name)
                 self.get_dependents(dep)
+
          return
+
      
-        
-    def initialize_dependencies_status(self, action):
-         
+    def initialize_dependencies_status(self, action):      
+
          ''' initialize the status of the actions with no deps as ready '''
-         
+
          if self.current_action_dependencies[action] is None:
-             self.current_action_status[action] = 'ready'
+             self.current_action_status[action] = Status.ready
+
          else:
              for dep in self.current_action_dependencies[action]:                          
                  self.initialize_dependencies_status(dep)                     
-         return
-     
+
+         return                
+
+              
+class ActionExecutor():
+
+    def __init__(self, root_dir):
+
+        self.root_dir = root_dir
+        self.ongoing_subprocesses = []
+        self.action_name_for_ongoing_subprocess = []
+        self.action_name = ''
+        self.pending_actions = []
+
         
+    def execute(self, build, action):
+
+        if build != 'build':
+            raise Exception('Command not recognized.')
+            
+        Action_obj = Action()
+        self.current_action_status, self.current_action_commands, self.current_action_dependencies, self.dependents = Action_obj.action_sequence(self.root_dir, build, action)
+        self.initialize_pending_actions()
+        self.execute_commands()
+
+        return
+    
+    
+    def initialize_pending_actions(self):
+
+        for action in self.current_action_status:
+            self.pending_actions.append(action)
+
+        return
+
+
     def update_dependencies_status(self, action):
-         
-         ''' updates the status of the actions with all deps executed as ready '''
-        
+
+         ''' updates the status of the actions with all deps executed as ready '''        
+
          total_no_of_dependencies = len(self.current_action_dependencies[action])
          no_of_dependencies_done = 0
          for dep in self.current_action_dependencies[action]:         
-             if self.current_action_status[dep] == 'done':
+             if self.current_action_status[dep] == Status.done:
                  no_of_dependencies_done += 1
          if no_of_dependencies_done == total_no_of_dependencies:
-             self.current_action_status[action] = 'ready'
+             self.current_action_status[action] = Status.ready
+
          return
-     
-     
+
+
     def execute_commands(self):
-           
+
         while len(self.pending_actions) != 0:
-            action_has_no_command = False
+            any_action_has_no_command = False
             for name in self.current_action_status:
-                 if self.current_action_status[name] == 'ready':
-                     self.current_action_status[name] = 'processing'
+                 if self.current_action_status[name] == Status.ready:
+                     self.current_action_status[name] = Status.processing
                      cwd = os.getcwd()
                      if '/' in name:
                          loc = name[:name.rindex('/')]
@@ -161,42 +188,43 @@ class ActionExecutor():
                      if loc != '':
                          cwd = cwd+os.path.sep+loc
                      command = self.current_action_commands[name]
-                     
                      if command is None:                         
-                         action_has_no_command = True
-                         self.current_action_status[name] = 'done'
+                         any_action_has_no_command = True
+                         self.current_action_status[name] = Status.done
                          self.pending_actions.remove(name)
                          if len(self.dependents[name]) != 0:
                              for action in self.dependents[name]:
-                                 if self.current_action_status[action] == 'not started':
+                                 if self.current_action_status[action] == Status.not_started:
                                      self.update_dependencies_status(action)
                          break
-                     
                      else:
-                         p = subprocess.Popen(command, shell=True, cwd=cwd)
-                         self.ongoing_subprocesses.append(p)
+                         process = subprocess.Popen(command, shell=True, cwd=cwd)
+                         self.ongoing_subprocesses.append(process)
                          self.action_name_for_ongoing_subprocess.append(name)
-                         
-            if action_has_no_command:
+
+            if any_action_has_no_command:
                 continue
-               
+
             while True:                
                  any_subprocess_completed = False                
-                 for p, action in zip(self.ongoing_subprocesses, self.action_name_for_ongoing_subprocess):
-                     if not p.poll() is None:
+                 for process, action in zip(self.ongoing_subprocesses, self.action_name_for_ongoing_subprocess):
+                     if not process.poll() is None:
                          any_subprocess_completed = True
                          break     
+
                  if any_subprocess_completed:
                      break
                  time.sleep(1)
-                            
-            self.ongoing_subprocesses.remove(p)
+
+            self.ongoing_subprocesses.remove(process)
             self.action_name_for_ongoing_subprocess.remove(action)
-            self.current_action_status[action] = 'done'
+            self.current_action_status[action] = Status.done
             self.pending_actions.remove(action)
+            
             if len(self.dependents[action]) != 0:
                 for name in self.dependents[action]:
-                    if self.current_action_status[name] == 'not started':
+                    if self.current_action_status[name] == Status.not_started:
                         self.update_dependencies_status(name)
+
         return 
     
--- solutions/Abhilasha/Assignment2/btest.py
+++ solutions/Abhilasha/Assignment2/btest.py
@@ -5,28 +5,28 @@ import tempfile
 import shutil
 import BuildAutomationTool as BAT
 
-class TestBuildAutomationTool(unittest.TestCase):
-      
+
+class TestBuildAutomationTool(unittest.TestCase):    
+
     def setUp(self):
-        
+
         self.root = os.getcwd()
         self.test_dir = tempfile.TemporaryDirectory()
         
-            
-    def TearDown(self):
+
+    def TearDown(self):   
+
+        shutil.rmtree(self.test_dir)        
         
-        shutil.rmtree(self.test_dir)
-    
-    
-    def test_build_test_all(self):
-        
+
+    def test_build_test_all_and_test_clean(self):  
+
         with self.test_dir as tmpdirname:
             new_root_dir = tmpdirname+os.path.sep+"code"
-            shutil.copytree(self.root+os.path.sep+"code", new_root_dir) 
-            os.chdir(new_root_dir)
-            BAT_obj = BAT.ActionGraph(new_root_dir)
-            BAT_obj.create_action_map()
-            BAT_obj.get_command_to_be_executed('build', 'test_all')
+            shutil.copytree(self.root+os.path.sep+"code", new_root_dir)
+            os.chdir(new_root_dir)            
+            executor = BAT.ActionExecutor(new_root_dir)
+            executor.execute('build', 'test_all')
             self.assertEqual(path.exists('test.o'), True)
             self.assertEqual(path.exists('test_sort_bubble.exe'), True)
             self.assertEqual(path.exists('test_sort_quick.exe'), True)
@@ -35,18 +35,9 @@ class TestBuildAutomationTool(unittest.TestCase):
             self.assertEqual(path.exists('sort_bubble.o'), True)
             self.assertEqual(path.exists('sort_quick.o'), True)
             self.assertEqual(path.exists('sort_merge.o'), True)
-            os.chdir(self.root)
             
-            
-    def test_clean(self):
-        
-        with self.test_dir as tmpdirname:
-            new_root_dir = tmpdirname+os.path.sep+"code"
-            shutil.copytree(self.root+os.path.sep+"code", new_root_dir)
-            os.chdir(new_root_dir)
-            BAT_obj = BAT.ActionGraph(new_root_dir)
-            BAT_obj.create_action_map()
-            BAT_obj.get_command_to_be_executed('build', 'clean')
+            os.chdir(new_root_dir)  
+            executor.execute('build', 'clean')
             self.assertEqual(path.exists('test.o'), False)
             self.assertEqual(path.exists('test_sort_bubble.exe'), False)
             self.assertEqual(path.exists('test_sort_quick.exe'), False)
@@ -56,17 +47,16 @@ class TestBuildAutomationTool(unittest.TestCase):
             self.assertEqual(path.exists('sort_quick.o'), False)
             self.assertEqual(path.exists('sort_merge.o'), False)  
             os.chdir(self.root)
-                
         
+
     def test_sort_merge(self):
-        
+
         with self.test_dir as tmpdirname:
             new_root_dir = tmpdirname+os.path.sep+"code"
             shutil.copytree(self.root+os.path.sep+"code", new_root_dir)
-            os.chdir(new_root_dir)
-            BAT_obj = BAT.ActionGraph(new_root_dir)
-            BAT_obj.create_action_map()
-            BAT_obj.get_command_to_be_executed('build', 'test_sort_merge')       
+            os.chdir(new_root_dir)            
+            executor = BAT.ActionExecutor(new_root_dir)  
+            executor.execute('build', 'test_sort_merge')
             self.assertEqual(path.exists('test.o'), True)
             self.assertEqual(path.exists('test_sort_merge.exe'), True)
             self.assertEqual(path.exists('test_sort_bubble.exe'), False)
@@ -76,22 +66,20 @@ class TestBuildAutomationTool(unittest.TestCase):
             self.assertEqual(path.exists('sort_bubble.o'), False)
             self.assertEqual(path.exists('sort_quick.o'), False)
             os.chdir(self.root)
+
         
-        
-    def test_invalid_key(self):
-       
+    def test_invalid_key(self):       
+
         with self.test_dir as tmpdirname:
             new_root_dir = tmpdirname+os.path.sep+"code"
             shutil.copytree(self.root+os.path.sep+"code", new_root_dir) 
-            os.chdir(new_root_dir)
-            BAT_obj = BAT.ActionGraph(new_root_dir)
-            BAT_obj.create_action_map()
+            os.chdir(new_root_dir)            
+            executor = BAT.ActionExecutor(new_root_dir)              
             with self.assertRaisesRegex(Exception, 'Command not recognized.'):
-                BAT_obj.get_command_to_be_executed('build', 'test_sort_selection')
+                executor.execute('build', 'test_sort_selection')
             os.chdir(self.root)
-                
+
 
 if __name__ == '__main__':
 
     unittest.main()
-    
